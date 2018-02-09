b955e9b1e955a77a81fc961e96f8351ed8c54e1b
yuchen_wang <yuchen.wang@intel.com>
2018-02-09 08:56:51 +0800

    Refine cmake file in OS(CL#737866)
    Change-Id: Ifb95c213e3c6b90825bbe55f75bf95b5324bac01

--- media_driver/agnostic/common/os/media_srcs.cmake
+++ media_driver/agnostic/common/os/media_srcs.cmake
@@ -27,6 +27,8 @@ set(TMP_SOURCES_
     ${CMAKE_CURRENT_LIST_DIR}/mos_utilities.c
     ${CMAKE_CURRENT_LIST_DIR}/mos_gpucontext.cpp
     ${CMAKE_CURRENT_LIST_DIR}/mos_gpucontextmgr.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/mos_cmdbufmgr.cpp
+    ${CMAKE_CURRENT_LIST_DIR}/mos_commandbuffer.cpp
 )
 
 set(TMP_HEADERS_
@@ -45,21 +47,9 @@ set(TMP_HEADERS_
     ${CMAKE_CURRENT_LIST_DIR}/mos_utilities.h
     ${CMAKE_CURRENT_LIST_DIR}/mos_gpucontext.h
     ${CMAKE_CURRENT_LIST_DIR}/mos_gpucontextmgr.h
-)
-
-if(${PLATFORM} STREQUAL "linux" OR ${PLATFORM} STREQUAL "yocto" OR ${PLATFORM} STREQUAL "android")
-set(TMP_SOURCES_
-    ${TMP_SOURCES_}
-    ${CMAKE_CURRENT_LIST_DIR}/mos_cmdbufmgr.cpp
-    ${CMAKE_CURRENT_LIST_DIR}/mos_commandbuffer.cpp
-)
-
-set(TMP_HEADERS_
-    ${TMP_HEADERS_}
     ${CMAKE_CURRENT_LIST_DIR}/mos_cmdbufmgr.h
     ${CMAKE_CURRENT_LIST_DIR}/mos_commandbuffer.h
 )
-endif()
 
 set(SOURCES_
     ${SOURCES_}
