6ecf0d7e6f7124dc5a438768a731f66c2ff03c7b
weizhi_(walter)__bai <walter.bai@intel.com>
2018-03-06 11:38:17 +0800

    [Linux ULT] Add copyright and fix a compile warning for memory_leak_detector.cpp.(CL#746026)
    Change-Id: Ib4b19233da2eadfb2d06b800172d19950045c905
    Description: Add copyright and fix a compile warning for memory_leak_detector.cpp.

--- media_driver/linux/ult/ult_app/memory_leak_detector.cpp
+++ media_driver/linux/ult/ult_app/memory_leak_detector.cpp
@@ -1,3 +1,24 @@
+/*
+* Copyright (c) 2018, Intel Corporation
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*/
 #include "memory_leak_detector.h"
 #include "gtest/gtest.h"
 #include <fstream>
@@ -87,7 +108,7 @@ void MemoryLeakDetectorIpl::detect(const string &logPath)
             if (memInfo.type == MEM_INFO_TYPE_ALLOC_SYS || memInfo.type == MEM_INFO_TYPE_ALLOC_GFX
                 || (pos = m_memPtr.find(ptr)) == m_memPtr.end())
             {
-                m_memPtr[ptr] = m_memInfoTable.size();
+                m_memPtr[ptr] = static_cast<int32_t>(m_memInfoTable.size());
             }
             else
             {
