6f1df344cae53db19659a600b528b7d992d2e7fd
yue_(jackie)__guo <jackie.guo@intel.com>
2018-04-09 10:39:48 +0800

    Enable texture array usage with none-zero YOffsetForFrame in VEBOX_SURFACE_STATE.(CL#755441)
    Change-Id: Ic49bb50dc766b36721baa19b1f9e361e314e2cf5

--- media_driver/agnostic/common/vp/hal/vphal_render_vebox_util_base.c
+++ media_driver/agnostic/common/vp/hal/vphal_render_vebox_util_base.c
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2014-2017, Intel Corporation
+* Copyright (c) 2014-2018, Intel Corporation
 *
 * Permission is hereby granted, free of charge, to any person obtaining a
 * copy of this software and associated documentation files (the "Software"),
@@ -141,6 +141,7 @@ MOS_STATUS VpHal_InitVeboxSurfaceStateCmdParams(
         VPHAL_RENDER_CHK_STATUS(VpHal_InitVeboxSurfaceParams(
                                 pVpHalVeboxSurfaceStateCmdParams->pSurfInput,
                                 &pMhwVeboxSurfaceStateCmdParams->SurfInput));
+        pMhwVeboxSurfaceStateCmdParams->SurfInput.dwYoffset = pVpHalVeboxSurfaceStateCmdParams->pSurfInput->YPlaneOffset.iYOffset;
     }
     if (pVpHalVeboxSurfaceStateCmdParams->pSurfOutput)
     {
