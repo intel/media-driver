bc38e7886e365bc956dc859db8d16e34cc9c4f08
xiong_(sean)__zhou <sean.zhou@intel.com>
2018-05-31 15:18:01 +0800

    Fix segment fault crash when enabled surface dump.(CL#770423)
    Change-Id: Ica47ed9f81cdc2100b7a149242a4e5635f8c39e1

--- media_driver/agnostic/common/cm/cm_surface_manager.cpp
+++ media_driver/agnostic/common/cm/cm_surface_manager.cpp
@@ -1584,6 +1584,12 @@ int32_t CmSurfaceManager::DestroySurface( CmSurfaceVme* & vmeSurface )
 //*-----------------------------------------------------------------------------
 int32_t CmSurfaceManager::GetSurface( const uint32_t index, CmSurface* & surface )
 {
+    if( index == CM_NULL_SURFACE)
+    {
+        surface = nullptr;
+        return CM_FAILURE;
+    }
+
     if( index < m_surfaceArraySize )
     {
         surface = m_surfaceArray[ index ];
