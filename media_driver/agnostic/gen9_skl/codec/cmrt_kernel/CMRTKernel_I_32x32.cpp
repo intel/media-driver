c77909f2548f18e4c70e35caad88afece7f675f8
Stephanie_Qi <stephanie.qi@intel.com>
2018-07-01 17:08:23 +0800

    Modified create and destroy mechanism of MDF resources to fixed the memory growth issue.(CL#779068)
    Modified create and destroy mechanism of MDF resources to fixed the memory growth at rate in 0.5Gb per hour.
    
    Change-Id: I96c28ca6520d58babcbb301ea9e7fe8bfc8ff4a2

--- media_driver/agnostic/gen9_skl/codec/cmrt_kernel/CMRTKernel_I_32x32.cpp
+++ media_driver/agnostic/gen9_skl/codec/cmrt_kernel/CMRTKernel_I_32x32.cpp
@@ -126,7 +126,7 @@ CM_RETURN_CODE CMRTKernelI32x32::CreateAndDispatchKernel(CmEvent *&cmEvent, bool
 
     CM_CHK_STATUS_RETURN(m_cmKernel->SetThreadCount(threadSpaceWidth * threadSpaceHeight));
     //create Thread Space
-    result = m_cmDev->CreateThreadSpace(threadSpaceWidth, threadSpaceHeight, m_cmThreadSpace);
+    result = CreateThreadSpace(threadSpaceWidth, threadSpaceHeight);
     if (result != CM_SUCCESS)
     {
         printf("CM Create ThreadSpace error : %d", result);
