fb73485b0719357793cd5c95e83b31a8e81220b6
xiong_(sean)__zhou <sean.zhou@intel.com>
2018-07-06 09:59:13 +0800

    Fix 32-bit host app crash when memninja print is enabled.(CL#780748)
    Change-Id: I51ab5c686b7311bdc6fa21a9e3e863acdf6d091a

--- media_driver/agnostic/common/os/mos_utilities.h
+++ media_driver/agnostic/common/os/mos_utilities.h
@@ -142,22 +142,22 @@ extern uint8_t MosUltFlag;
 //! Helper Macros for MEMNINJA debug messages
 #define MOS_MEMNINJA_ALLOC_MESSAGE(ptr, size, functionName, filename, line)                                 \
     MOS_OS_VERBOSEMESSAGE(                                                                                  \
-        "MemNinjaSysAlloc: MemNinjaCounter = %d, memPtr = 0x%016llx, size = %d, functionName = \"%s\", "    \
+        "MemNinjaSysAlloc: MemNinjaCounter = %d, memPtr = 0x%p, size = %d, functionName = \"%s\", "         \
         "filename = \"%s\", line = %d/", MosMemAllocCounter, ptr, size, functionName, filename, line)
 
 #define MOS_MEMNINJA_FREE_MESSAGE(ptr, functionName, filename, line)                                        \
     MOS_OS_VERBOSEMESSAGE(                                                                                  \
-       "MemNinjaSysFree: MemNinjaCounter = %d, memPtr = 0x%016llx, functionName = \"%s\", "                 \
+       "MemNinjaSysFree: MemNinjaCounter = %d, memPtr = 0x%p, functionName = \"%s\", "                      \
        "filename = \"%s\", line = %d/", MosMemAllocCounter, ptr, functionName, filename, line)
 
 #define MOS_MEMNINJA_GFX_ALLOC_MESSAGE(ptr, size, functionName, filename, line)                             \
     MOS_OS_VERBOSEMESSAGE(                                                                                  \
-        "MemNinjaGfxAlloc: MemNinjaCounterGfx = %d, memPtr = 0x%016llx, size = %d, functionName = \"%s\", " \
+        "MemNinjaGfxAlloc: MemNinjaCounterGfx = %d, memPtr = 0x%p, size = %d, functionName = \"%s\", "      \
         "filename = \"%s\", line = %d/", MosMemAllocCounterGfx, ptr, size, functionName, filename, line)
 
 #define MOS_MEMNINJA_GFX_FREE_MESSAGE(ptr, functionName, filename, line)                                    \
     MOS_OS_VERBOSEMESSAGE(                                                                                  \
-        "MemNinjaGfxFree: MemNinjaCounterGfx = %d, memPtr = 0x%016llx, functionName = \"%s\", "             \
+        "MemNinjaGfxFree: MemNinjaCounterGfx = %d, memPtr = 0x%p, functionName = \"%s\", "                  \
         "filename = \"%s\", line = %d/", MosMemAllocCounterGfx, ptr, functionName, filename, line)
 
 //!
