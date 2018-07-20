9dec9c8c6fcf2ac4322d065d4739a5a12f8a674f
fan_he <fan.f.he@intel.com>
2018-07-20 11:43:33 +0800

    [CM] Remove redundant and invalid license headers(CL#784370)
    Remove redundant and invalid license headers
    
    Change-Id: I11434d17c84ae765c24d61fa6470c7ad6643243b

--- cmrtlib/agnostic/hardware/cm_surface_manager.cpp
+++ cmrtlib/agnostic/hardware/cm_surface_manager.cpp
@@ -20,17 +20,6 @@
 * OTHER DEALINGS IN THE SOFTWARE.
 */
 
-/**-----------------------------------------------------------------------------
-***
-*** Copyright  (C) 1985-2016 Intel Corporation. All rights reserved.
-***
-*** The information and source code contained herein is the exclusive
-*** property of Intel Corporation. and may not be disclosed, examined
-*** or reproduced in whole or in part without explicit written authorization
-*** from the company.
-***
-*** ----------------------------------------------------------------------------
-*/
 #include "cm_surface_manager.h"
 #include "cm_debug.h"
 #include "cm_mem.h"
