68a9e6356f1844cbe43f04b439a27a042f6fe0a3
jianhong_zhang <jianhong.zhang@intel.com>
2018-07-24 10:08:53 +0800

    [Encode] Min tile height = 128(CL#785168)
    Min tile height = 128
    
    Change-Id: I8e9db8c06498ee63c3574f2c01050cd59b6a8db6

--- media_driver/agnostic/common/codec/hal/codechal_vdenc_vp9_base.h
+++ media_driver/agnostic/common/codec/hal/codechal_vdenc_vp9_base.h
@@ -54,7 +54,8 @@
 #define CODECHAL_ENCODE_VP9_BRC_SUPER_FRAME_BUFFER_SIZE         MOS_ALIGN_CEIL(3 + 2 * sizeof(uint32_t), sizeof(uint32_t))
 #define CODECHAL_ENCODE_VP9_VDENC_DATA_EXTENSION_SIZE           32
 #define CODECHAL_ENCODE_VP9_PIC_STATE_BUFFER_SIZE_PER_PASS      192 // 42 DWORDs for Pic State one uint32_t for BB End + 5 uint32_tS reserved to make it aligned for kernel read
-#define CODECHAL_ENCODE_VP9_MIN_TILE_SIZE                       256
+#define CODECHAL_ENCODE_VP9_MIN_TILE_SIZE_WIDTH                 256
+#define CODECHAL_ENCODE_VP9_MIN_TILE_SIZE_HEIGHT                128
 #define CODECHAL_ENCODE_VP9_VDENC_MAX_NUM_TEMPORAL_LAYERS       8
 #define CODECHAL_ENCODE_VP9_HUC_SUPERFRAME_PASS                 2
 #define CODECHAL_ENCODE_VP9_REF_SEGMENT_DISABLED                0xFF
