63c5c6b8ad989f9c2c43bff2e03af6d8a013ead9
Yang, Ning <ning.yang@intel.com>
2018-08-08 11:50:41 +0800

    [Upstream] modify default building configuration
    Signed-off-by: Yang, Ning <ning.yang@intel.com>
    
    1. Remove unified_cmake.sh file
    2. add more flags in distro_cmake.sh

--- distro_cmake.sh
+++ distro_cmake.sh
@@ -7,5 +7,9 @@ cmake ../media-driver \
 -DGEN8_Supported="no" \
 -DGEN8_BDW_Supported="no" \
 -DGEN9_Supported="no" \
+-DGEN9_BXT_Supported="no" \
 -DGEN9_SKL_Supported="no" \
+-DGEN9_CFL_Supported="no" \
+-DGEN9_GLK_Supported="no" \
+-DGEN9_KBL_Supported="no" \
 -DFull_Open_Source_Support="yes" $@
