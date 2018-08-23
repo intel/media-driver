0d7a922fc444acf9d593c3fce5ab052c11154fb6
Sekuru_Karthik <karthik.sekuru@intel.com>
2018-08-23 13:36:00 +0800

    [Encode] Cabac zero enabling(CL#793496)
    Change-Id: Id43e63d8fae3b97b15f85c6b14c8c35af55380e6

--- media_driver/agnostic/gen11/codec/hal/codechal_encode_hevc_g11.h
+++ media_driver/agnostic/gen11/codec/hal/codechal_encode_hevc_g11.h
@@ -78,21 +78,6 @@ struct HucPakStitchDmemEncG11
 };
 
 //!
-//! \struct HucCommandData
-//! \brief  The struct of Huc commands data
-//!
-struct HucCommandData
-{
-    uint32_t        TotalCommands;       //!< Total Commands in the Data buffer
-    struct
-    {
-        uint16_t    ID;              //!< Command ID, defined and order must be same as that in DMEM
-        uint16_t    SizeOfData;      //!< data size in uint32_t
-        uint32_t    data[40];
-    } InputCOM[10];
-};
-
-//!
 //! \struct HucInputCmdG11
 //! \brief  The struct of Huc input command
 //!
