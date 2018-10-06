c6ec06f5ad362bfc25709db62317acd9e0f588d9
xinfeng_(carl)__zhang <carl.zhang@intel.com>
2018-10-06 12:07:06 +0800

    [Decode] revert 0da5d5de/CL#801632 , revert 53dCL#304115c3faaa3a2c91c16afCL#079214ebc7d9e1(CL#805197)
    revert the change, because VP call in vaGetImage will bring in some issues with some formats.
    
    Change-Id: I4587d3e591a748db5ac445d43505bb509a86a612

--- media_driver/linux/common/ddi/media_libva_util.h
+++ media_driver/linux/common/ddi/media_libva_util.h
@@ -335,26 +335,6 @@ int32_t  DdiMediaUtil_OpenGraphicsAdaptor(char *devName);
 //!
 VAStatus DdiMediaUtil_UnRegisterRTSurfaces(VADriverContextP    ctx,PDDI_MEDIA_SURFACE surface);
 
-//!
-//! \brief get expected surface height and tile format
-//!
-//! \param [in] format
-//!     expect media format
-//! \param [in] surface_hint
-//!     surface hint
-//! \param [in] height
-//!     height;
-//! \param [out] aligned_height
-//!     expected aligned height
-//! \param [out] tile_format
-//!     expected tile format
-//!
-VAStatus DdiMediaUtil_GetExpectTileAndHeight(
-    DDI_MEDIA_FORMAT format,
-    uint32_t surface_hint,
-    int32_t height,
-    int32_t *aligned_height,
-    uint32_t *tile_format);
 //------------------------------------------------------------------------------
 // Macros for debug messages, Assert, Null check and condition check within ddi files
 //------------------------------------------------------------------------------
