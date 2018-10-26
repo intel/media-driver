4d4e3b37eab4e4330fc84608d669f1e288b9a3b6
shiqiang_Yu <shiqiang.yu@intel.com>
2018-10-26 11:34:29 +0800

    [Decode] Remove nullptr check for attribList if numAttribs is zero(CL#810801)
    Remove nullptr check for attribList if numAttribs is zero
    
    Change-Id: Id86d40601045d6e5d062bcde35ec326d031fbbc4

--- media_driver/linux/common/ddi/media_libva_caps.cpp
+++ media_driver/linux/common/ddi/media_libva_caps.cpp
@@ -1656,9 +1656,12 @@ VAStatus MediaLibvaCaps::CreateDecConfig(
         int32_t numAttribs,
         VAConfigID *configId)
 {
-    
-    DDI_CHK_NULL(attribList, "Null pointer", VA_STATUS_ERROR_INVALID_PARAMETER);
     DDI_CHK_NULL(configId, "Null pointer", VA_STATUS_ERROR_INVALID_PARAMETER);
+
+    if (numAttribs)
+    {
+        DDI_CHK_NULL(attribList, "Null pointer", VA_STATUS_ERROR_INVALID_PARAMETER);
+    }
 
     VAConfigAttrib decAttributes[3];
 
@@ -1714,8 +1717,12 @@ VAStatus MediaLibvaCaps::CreateEncConfig(
         int32_t numAttribs,
         VAConfigID *configId)
 {
-    DDI_CHK_NULL(attribList, "Null pointer", VA_STATUS_ERROR_INVALID_PARAMETER);
     DDI_CHK_NULL(configId, "Null pointer", VA_STATUS_ERROR_INVALID_PARAMETER);
+
+    if (numAttribs)
+    {
+        DDI_CHK_NULL(attribList, "Null pointer", VA_STATUS_ERROR_INVALID_PARAMETER);
+    }
 
     uint32_t rcMode = VA_RC_CQP;
     if((entrypoint == VAEntrypointStats) || (entrypoint == VAEntrypointEncPicture))
