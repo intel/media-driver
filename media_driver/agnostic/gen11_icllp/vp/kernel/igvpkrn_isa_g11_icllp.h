ffe148cf79fd611a463911b5dce82e07886834ec
furongzh <furong.zhang@intel.com>
2018-11-05 18:16:49 +0800

    [VP] ICLLP HDR open source enabling: VPHAL part
    ICLLP HDR open source enabling: VPHAL part
    
    Change-Id: If87dbdaf3d32ec761638af0c48f2ee00bc3d4ebc

---
+++ media_driver/agnostic/gen11_icllp/vp/kernel/igvpkrn_isa_g11_icllp.h
@@ -0,0 +1,33 @@
+/*
+* Copyright (c) 2018, Intel Corporation
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*/
+//!
+//! \file     igvpkrn_isa_g11_icllp.h
+//! \brief    Head File for Gen11 LP ISA
+//! \details  Head File for Gen11 LP ISA
+//!
+#ifndef __IGVPKRN_ISA_G11_ICLLP_H__
+#define __IGVPKRN_ISA_G11_ICLLP_H__
+
+extern const unsigned int IGVP3DLUT_GENERATION_G11_ICLLP_SIZE;
+extern const unsigned int IGVP3DLUT_GENERATION_G11_ICLLP[];
+
+#endif // __IGVPKRN_G11_ICLLP_H__
