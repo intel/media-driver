77afdb0a08c81065f7dfbe3fb19265bfa2cbcd87
XinfengZhang <carl.zhang@intel.com>
2018-11-14 22:22:29 +0800

    refacor MOS folder structure
    1. rename libdrm folder to i915
    2. add uapi folder to include upi headers from upstreamed KMD
    
    Signed-off-by: XinfengZhang <carl.zhang@intel.com>

--- media_driver/linux/common/os/i915/include/media_srcs.cmake
+++ media_driver/linux/common/os/i915/include/media_srcs.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2018, Intel Corporation
+# Copyright (c) 2017, Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -17,14 +17,36 @@
 # OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
-cmake_minimum_required(VERSION 2.8)
 
-project(libdrm_mock)
+media_include_subdirectory(uapi)
 
-include_directories(../../common/os/i915/include ../inc)
-aux_source_directory(. SOURCES)
-set_source_files_properties(${SOURCES} PROPERTIES LANGUAGE "CXX")
-add_library(drm_mock SHARED ${SOURCES})
+set(TMP_SOURCES_ "")
 
-target_link_libraries(drm_mock pciaccess)
-set_target_properties(drm_mock PROPERTIES VERSION 2.4.0 SOVERSION 2)
+set(TMP_HEADERS_
+    ${CMAKE_CURRENT_LIST_DIR}/intel_aub.h
+    ${CMAKE_CURRENT_LIST_DIR}/intel_chipset.h
+    ${CMAKE_CURRENT_LIST_DIR}/libdrm_lists.h
+    ${CMAKE_CURRENT_LIST_DIR}/libdrm_macros.h
+    ${CMAKE_CURRENT_LIST_DIR}/mos_bufmgr.h
+    ${CMAKE_CURRENT_LIST_DIR}/mos_bufmgr_priv.h
+    ${CMAKE_CURRENT_LIST_DIR}/xf86atomic.h
+    ${CMAKE_CURRENT_LIST_DIR}/xf86drm.h
+    ${CMAKE_CURRENT_LIST_DIR}/xf86drmHash.h
+    ${CMAKE_CURRENT_LIST_DIR}/xf86drmMode.h
+    ${CMAKE_CURRENT_LIST_DIR}/xf86drmRandom.h
+)
+
+# no source in this folder
+#set(SOURCES_
+#    ${SOURCES_}
+#    ${TMP_SOURCES_}
+#)
+
+set(HEADERS_
+    ${HEADERS_}
+    ${TMP_HEADERS_}
+)
+
+
+# use 'BEFORE' as below to make this folder higher priority in include path. This could avoid libdrm headers in system path are used unproperly.
+include_directories(BEFORE ${CMAKE_CURRENT_LIST_DIR})
