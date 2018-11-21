24da5b9d44c00d03adc20225ea1107778735fea9
marcmao <marc.mao@intel.com>
2018-11-21 20:20:07 +0800

    [Encode] Move SurfaceParams setting to function for DS kernel
    Move SurfaceParams of DSKernerl to function so it can be reused by for
    subclass
    
    Change-Id: I626509408481778bb928894236214a8b544bff81

--- media_driver/agnostic/common/codec/hal/codechal_encode_csc_ds.h
+++ media_driver/agnostic/common/codec/hal/codechal_encode_csc_ds.h
@@ -521,6 +521,7 @@ protected:
         uint32_t                dwMBVProcStatsBottomFieldOffset = 0;
         bool                    bCurrPicIsFrame = false;
         bool                    bPreEncInUse = false;
+        bool                    bEnable8x8Statistics = false;
     };
 
     //!
@@ -543,6 +544,17 @@ protected:
     //!           MOS_STATUS_SUCCESS if success, else fail reason
     //!
     virtual MOS_STATUS InitKernelStateDS();
+
+    //!
+    //! \brief    Set SurfaceParamsDS for DS kernel
+    //!
+    //! \param    params
+    //!           KernelParams pointer from caller
+    //!
+    //! \return   MOS_STATUS
+    //!           MOS_STATUS_SUCCESS if success, else fail reason
+    //!
+    MOS_STATUS SetSurfaceParamsDS(KernelParams* params);
 
     //!
     //! \brief    Setup Curbe for DS kernel
