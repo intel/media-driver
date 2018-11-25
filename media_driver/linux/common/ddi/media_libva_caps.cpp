25c308e14d22f31bce27c8604229a5f050c1af74
Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>
2018-11-25 21:45:54 +0800

    Revert "Add VP8 4K decoder attribute support"
    This reverts commit a46cc51bc85fd23f4de4d3dedef696b2a196952b.

--- media_driver/linux/common/ddi/media_libva_caps.cpp
+++ media_driver/linux/common/ddi/media_libva_caps.cpp
@@ -856,7 +856,7 @@ VAStatus MediaLibvaCaps::CreateDecAttributes(
     {
         attrib.value = ENCODE_JPEG_MAX_PIC_WIDTH;
     }
-    if(IsAvcProfile(profile)||IsHevcProfile(profile)||IsVp9Profile(profile)||IsVp8Profile(profile))
+    if(IsAvcProfile(profile)||IsHevcProfile(profile)||IsVp9Profile(profile))
     {
         attrib.value = CODEC_4K_MAX_PIC_WIDTH;
     }
@@ -868,7 +868,7 @@ VAStatus MediaLibvaCaps::CreateDecAttributes(
     {
         attrib.value = ENCODE_JPEG_MAX_PIC_HEIGHT;
     }
-    if(IsAvcProfile(profile)||IsHevcProfile(profile)||IsVp9Profile(profile)||IsVp8Profile(profile))
+    if(IsAvcProfile(profile)||IsHevcProfile(profile)||IsVp9Profile(profile))
     {
         attrib.value = CODEC_4K_MAX_PIC_HEIGHT;
     }
