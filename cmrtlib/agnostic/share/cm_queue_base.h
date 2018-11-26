5388082ed66df1e42957b9b2a0de280f36f7b724
dai1 <dai1@intel.com>
2018-11-26 14:49:39 +0800

    [CM][SIM]Enable_Multiple_Context
    Enable Multiple Context task dispatching
    
    Change-Id: I0977d99b3ec310c8b440e695854bcd5082d04357

--- cmrtlib/agnostic/share/cm_queue_base.h
+++ cmrtlib/agnostic/share/cm_queue_base.h
@@ -52,7 +52,7 @@ struct CM_QUEUE_CREATE_OPTION
 {
     CM_QUEUE_TYPE QueueType : 3;
     bool RunAloneMode       : 1;
-    unsigned int Reserved0  : 3;
+    unsigned int IsMultipleContextCase  : 3;  // for sim mode
     bool UserGPUContext     : 1;
     unsigned int GPUContext : 8; // user provided GPU CONTEXT in enum MOS_GPU_CONTEXT, this will override CM_QUEUE_TYPE if set
     CM_QUEUE_SSEU_USAGE_HINT_TYPE SseuUsageHint : 3;
