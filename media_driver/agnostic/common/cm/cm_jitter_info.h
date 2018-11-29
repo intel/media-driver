01ffbe9d955a6978cfa849f78ae0196228a3ab32
HeFan <fan.f.he@intel.com>
2018-11-29 10:49:27 +0800

    [CM] Change CM_JIT_INFO back to align with IGC, add reviewers in visa
    This reverts commit 44f690ec327b5e283c11b1010703029594618476.
    And add reviewers.yml file in visa/include folder, so if there is another change there, we will make corresponding changes.
    
    Change-Id: I7038780728a162ca65291f64cf098a07efff4a15

--- media_driver/agnostic/common/cm/cm_jitter_info.h
+++ media_driver/agnostic/common/cm/cm_jitter_info.h
@@ -30,6 +30,12 @@ typedef struct _CM_PROFILE_INFO {
     int value;
 } CM_PROFILE_INFO;
 
+typedef struct _CM_BB_INFO {
+    int id;
+    unsigned staticCycle;
+    unsigned sendStallCycle;
+} CM_BB_INFO;
+
 typedef struct _CM_JIT_INFO {
     // Common part
     bool isSpill;
@@ -53,9 +59,8 @@ typedef struct _CM_JIT_INFO {
     // whether kernel uses a barrier
     bool usesBarrier;
 
-    // (very) rough estimate of single thread execution/stall cycles
-    unsigned staticCycle;
-    unsigned sendStallCycle;
+    unsigned bbNum;
+    CM_BB_INFO *bbInfo;
 
     // number of spill/fill, weighted by loop
     unsigned int numGRFSpillFill;
