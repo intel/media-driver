8f51410015715408af12148847992c8c08c50a87
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-12-07 06:59:37 -0800

    build: provide BUILD_KERNELS option to rebuild shaders from sources
    Configuring as:
      cmake -DFREE_KERNELS=ON -DBUILD_KERNELS=ON ...
    it is now possible to trigger shaders rebuild from sources.
    
    As of now this would mean rebuild of VP gen11lp shaders only.
    
    Mind that this commit may not (and actually does not) lead to the
    successful build because in current driver architecture all platforms
    share the same vpkrnheader.h shaders header file which was
    pre-generated for exisiting non-free shaders (i.e. shaders without
    published sources). This header file gets regenerated within
    BUILD_KERNELS=ON procedure, but it eventually contains less definitions
    than non-free version. As a result driver compilation fails since usages
    of additional non-free definitions were not protected by any define.
    
    Change-Id: Icfbc53b6961a19cbdb8f75e70aa4424079f2fd8f
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- media_driver/agnostic/media_srcs.cmake
+++ media_driver/agnostic/media_srcs.cmake
@@ -18,6 +18,78 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
+if (BUILD_KERNELS)
+    # Here we define build steps to generate c-array binary shaders (kernels)
+    # and their header files. If you don't use BUILD_KERNELS option these
+    # kernels will just be used from pre-built form. If you will regenerate
+    # kernels you may notice the difference from the per-built kernels in git-diff.
+
+    # iga64 -i <src> -o <dst> -p <genx> -a
+    function(iga_compile src dst genx)
+        get_filename_component(tgt ${src} NAME)
+        add_custom_command(
+          OUTPUT ${dst}
+          DEPENDS KrnToHex_IGA ${src}
+          COMMAND ${IGA} -p ${genx} -a ${src} -o ${tgt}.krn
+          COMMAND KrnToHex_IGA ${tgt}.krn
+          COMMAND ${CMAKE_COMMAND} -E remove ${tgt}.krn
+          COMMAND ${CMAKE_COMMAND} -E rename ${tgt}.hex ${dst})
+    endfunction()
+
+    function(platform_to_genx platform genx kind)
+        if(platform STREQUAL "gen11_icllp")
+            set(genx "11" PARENT_SCOPE)
+            set(kind "icllp" PARENT_SCOPE)
+        endif()
+    endfunction()
+
+    # Function generate kernel for the specified platform. It assumes
+    # that kernel sources are located in a certain location (see ${krn_dir})
+    # calculated in a function body. And it assumes that output files should
+    # be stored in a certain locations (see ${krn_dir}/${krn}.h/c and ${krn_header}).
+    # Notes:
+    #   - <platform> = gen<X>_<kind>, for example, gen11_icllp
+    #   - As of now this function works for the single platform only (gen11_icllp) while it
+    #  actully should process all the platforms at once. This single processing
+    #  is needed to generate shared ${krn_header} with the unified kernels definition.
+    function(gen_kernel_from_asm name platform)
+        platform_to_genx(${platform} genx kind)
+
+        set(krn_dir ${CMAKE_SOURCE_DIR}/media_driver/agnostic/${platform}/vp/kernel)
+        set(link_file ${krn_dir}/component_release/LinkFile.txt)
+        set(krn ig${name}krn_g${genx}_${kind})
+        set(krn_header ${CMAKE_CURRENT_LIST_DIR}/common/vp/kernel/${name}krnheader.h)
+
+        # Compiling all the sources in the kernel source directory
+        file(GLOB srcs ${krn_dir}/Source/*.asm)
+        set(hexs "")
+        foreach(src ${srcs})
+            get_filename_component(tgt ${src} NAME_WE)
+            iga_compile(${src} ${tgt}.hex ${genx})
+            list(APPEND hexs ${tgt}.hex)
+        endforeach()
+
+        # Generating common .bin file containing all the kernels and its header
+        # file shared across all platforms (${name}krnheader.h)
+        add_custom_command(
+            OUTPUT ${krn}.bin ${krn_header}
+            DEPENDS GenKrnBin ${hexs} ${link_file}
+            COMMAND ${CMAKE_COMMAND} -E copy ${link_file} ./
+            COMMAND GenKrnBin . ${name}
+            COMMAND ${CMAKE_COMMAND} -E copy ${krn}.h ${krn_header})
+
+        # Generating kernel from the .bin file
+        add_custom_command(
+            OUTPUT ${krn_dir}/${krn}.c ${krn_dir}/${krn}.h
+            DEPENDS KernelBinToSource ${krn}.bin
+            COMMAND KernelBinToSource -i ${krn}.bin -o ${krn_dir}/)
+    endfunction()
+
+    if(GEN11_ICLLP)
+        gen_kernel_from_asm(vp gen11_icllp)
+    endif()
+endif()
+
 media_include_subdirectory(common)
 
 if(GEN8)
