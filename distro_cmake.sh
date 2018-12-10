0420f25c5f8cb27793340763c42d8303984c4dc2
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-12-10 06:51:09 -0800

    build: refactor Full_Open_Source_Support to a proper option
    This commit refactors Full_Open_Source_Support to a standard cmake
    option named FREE_KERNELS.
    
    Change-Id: Ia697216167a5cf6030dbc23093449e5c99e17216
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- distro_cmake.sh
+++ distro_cmake.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Full_Open_Source_Support flag is to control full-open-source build.
-# Full-open-source means only using open source EU kernels and fixed function hardware.
-
 cmake ../media-driver \
 -DGEN8_Supported="no" \
 -DGEN8_BDW_Supported="no" \
@@ -12,4 +9,4 @@ cmake ../media-driver \
 -DGEN9_CFL_Supported="no" \
 -DGEN9_GLK_Supported="no" \
 -DGEN9_KBL_Supported="no" \
--DFull_Open_Source_Support="yes" $@
+-DFREE_KERNELS=ON $@
