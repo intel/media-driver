84c5a4785cc6bb30287cede63c6dbe00780b6899
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-12-10 06:51:09 -0800

    build: refactor Full_Open_Source_Support to a proper option
    This commit refactors Full_Open_Source_Support to a standard cmake
    option named FREE_KERNELS.
    
    Change-Id: Ia697216167a5cf6030dbc23093449e5c99e17216
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- media_driver/agnostic/gen10/codec/media_srcs.cmake
+++ media_driver/agnostic/gen10/codec/media_srcs.cmake
@@ -19,7 +19,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 media_include_subdirectory(hal)
-if(NOT "${Full_Open_Source_Support}" STREQUAL "yes")
+if(NOT FREE_KERNELS)
     media_include_subdirectory(kernel)
 endif()
 media_include_subdirectory(share)
