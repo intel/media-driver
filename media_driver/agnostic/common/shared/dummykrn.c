71d16a29a320f48720c41eef3edbd9ff0b9d6d8f
Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>
2018-12-12 06:49:39 -0800

    gen8: join handling of stripped kernels for different build options
    ENABLE_KERNELS=OFF and FREE_KERNELS=ON has intersection: they both
    exclude some kernels from the build. This there is no much sense
    to exclude such kernels differently. We can join handling of that
    and ease project support. Doing the simplification for Gen8.
    
    Change-Id: I0d216742985bfb2a425d3c54327e187d62ca1e22
    Signed-off-by: Dmitry Rogozhkin <dmitry.v.rogozhkin@intel.com>

--- media_driver/agnostic/common/shared/dummykrn.c
+++ media_driver/agnostic/common/shared/dummykrn.c
@@ -25,15 +25,6 @@ const unsigned int IGCDUMMYKRN_SIZE = 0;
 const unsigned int IGCDUMMYKRN[] = {
 };
 
-//gen8 dummy kernel
-extern const unsigned int IGCODECKRN_G8_SIZE = IGCDUMMYKRN_SIZE;
-extern const unsigned int* IGCODECKRN_G8;
-const unsigned int* IGCODECKRN_G8 = IGCDUMMYKRN;
-
-extern const unsigned int IGVPKRN_G8_SIZE = IGCDUMMYKRN_SIZE;
-extern const unsigned int* IGVPKRN_G8;
-const unsigned int* IGVPKRN_G8 = IGCDUMMYKRN;
-
 //gen9 dummy kernel
 extern const unsigned int IGCODECKRN_G9_SIZE = IGCDUMMYKRN_SIZE;
 extern const unsigned int* IGCODECKRN_G9;
