e7fc12d5fb7e0e82524cb7bb4ac70d5523eb22d8
Zhou_Sean <sean.zhou@intel.com>
2018-12-25 16:29:51 +0800

    [CM] Fix gcc8 build issue.
    Change-Id: Id460833dfeceb5e32133c9e93745fc553094fd23

--- media_driver/agnostic/common/hw/mhw_render.h
+++ media_driver/agnostic/common/hw/mhw_render.h
@@ -131,10 +131,10 @@ typedef struct _MHW_STATE_BASE_ADDR_PARAMS
     uint32_t                dwIndirectObjectBufferSize;
     PMOS_RESOURCE           presInstructionBuffer;
     uint32_t                dwInstructionBufferSize;
-    uint32_t                mocs4InstructionCache =0;
-    uint32_t                mocs4GeneralState =0;
-    uint32_t                mocs4DynamicState =0;
-    uint32_t                mocs4SurfaceState =0;
+    uint32_t                mocs4InstructionCache;
+    uint32_t                mocs4GeneralState;
+    uint32_t                mocs4DynamicState;
+    uint32_t                mocs4SurfaceState;
 } MHW_STATE_BASE_ADDR_PARAMS, *PMHW_STATE_BASE_ADDR_PARAMS;
 
 typedef struct _MHW_VFE_SCOREBOARD_DELTA
