7ed6dabfab591dc9da8d18c9856a6fbd61840ff3
bowang1 <bo.b.wang@intel.com>
2019-01-10 11:46:20 +0800

    [Decode]Mark reference surface of IBC mode writable
    Change-Id: Iff2eea2034936dd2b124a57739adf6040ec4c2c5

--- media_driver/agnostic/common/hw/vdbox/mhw_vdbox.h
+++ media_driver/agnostic/common/hw/vdbox/mhw_vdbox.h
@@ -345,6 +345,7 @@ struct MHW_VDBOX_PIPE_BUF_ADDR_PARAMS
     uint32_t                    dwVdencStatsStreamOutOffset = 0;
     bool                        oneOnOneMapping = false;                 // Flag for indicating using 1:1 ref index mapping for vdenc
     bool                        isLowDelayB = true;                      // Flag to indicate if it is LDB
+    uint8_t                     IBCRefIdxMask = 0;
     virtual ~MHW_VDBOX_PIPE_BUF_ADDR_PARAMS() {}
 };
 using PMHW_VDBOX_PIPE_BUF_ADDR_PARAMS = MHW_VDBOX_PIPE_BUF_ADDR_PARAMS * ;
