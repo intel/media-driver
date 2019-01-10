f59e771be17dc31308e66fe25631c132b3afcedc
Chen_JasonK <jason.k.chen@intel.com>
2019-01-10 11:01:02 +0800

    Revert "[VP]correct comments on Render"
    This reverts commit 597ee400c9a71978c1dd3902c7f441f797defd96.
    Change-Id: Id394313a73afe25a442659e2a1716442cfe6b6cf

--- media_driver/agnostic/gen11/vp/hal/vphal_renderer_g11.h
+++ media_driver/agnostic/gen11/vp/hal/vphal_renderer_g11.h
@@ -46,7 +46,7 @@ class VphalRendererG11 : public VphalRenderer
 {
 public:
     //!
-    //! \brief    VphalRendererG11 constructor
+    //! \brief    VphalRendererG8 constructor
     //! \details  Based on the HW and OS info, initialize the renderer interfaces
     //! \param    [in,out] pRenderHal
     //!           Pointer to RenderHal Interface Structure
