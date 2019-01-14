a138fd3a65cde87f9113be2354d49bcd69b9a862
Zhou_Sean <sean.zhou@intel.com>
2019-01-14 15:16:09 +0800

    [CM] Refine CM_QUEUE_CREATE_OPTION for open source.
    Change-Id: Ifc6e339b6365f6cc840943f10873bdab3429867c

--- cmrtlib/agnostic/hardware/cm_queue.cpp
+++ cmrtlib/agnostic/hardware/cm_queue.cpp
@@ -28,12 +28,9 @@
 
 struct CM_CREATEQUEUE_PARAM
 {
-    unsigned int cmQueueType;   // [in]
-    bool cmRunAloneMode;        // [in]
-    unsigned int cmGPUContext;  // [in]
-    unsigned int cmSSEUUsageHint; // [in]
-    void *cmQueueHandle;        // [out]
-    int32_t returnValue;        // [out]
+    CM_QUEUE_CREATE_OPTION createOption; // [in/out]
+    void *cmQueueHandle;                 // [out]
+    int32_t returnValue;                 // [out]
 };
 
 struct CM_ENQUEUE_PARAM
@@ -178,10 +175,7 @@ int32_t CmQueue_RT::Initialize()
     CHK_FAILURE_RETURN(hr);
     CHK_FAILURE_RETURN(inParam.returnValue);
     m_cmQueueHandle = inParam.cmQueueHandle;
-    m_queueOption.QueueType = (CM_QUEUE_TYPE)inParam.cmQueueType;
-    m_queueOption.RunAloneMode = inParam.cmRunAloneMode;
-    m_queueOption.GPUContext = inParam.cmGPUContext;
-    m_queueOption.SseuUsageHint = (CM_QUEUE_SSEU_USAGE_HINT_TYPE)inParam.cmSSEUUsageHint;
+    m_queueOption   = inParam.createOption;
     return CM_SUCCESS;
 }
 
@@ -189,10 +183,7 @@ int32_t CmQueue_RT::Initialize(CM_QUEUE_CREATE_OPTION queueCreateOption)
 {
     CM_CREATEQUEUE_PARAM inParam;
     CmSafeMemSet(&inParam, 0, sizeof(inParam));
-    inParam.cmQueueType = queueCreateOption.QueueType;
-    inParam.cmRunAloneMode = queueCreateOption.RunAloneMode;
-    inParam.cmGPUContext = queueCreateOption.GPUContext;
-    inParam.cmSSEUUsageHint = queueCreateOption.SseuUsageHint;
+    inParam.createOption = queueCreateOption;
 
     int32_t hr = m_cmDev->OSALExtensionExecute(CM_FN_CMDEVICE_CREATEQUEUEEX,
                                                 &inParam, sizeof(inParam));
