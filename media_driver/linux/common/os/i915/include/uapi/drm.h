fdf8733a0b38c0d76cb6f8867ca460bf573bb5fa
XinfengZhang <carl.zhang@intel.com>
2019-01-28 11:24:05 -0500

    replace the drm headers which is generated by make headers_install
    the uAPI headers is from drm-intel, commit id is still 9235dd441af43599b9cdcce599a3da4083fcad3c
    Fixes #504
    
    Signed-off-by: XinfengZhang <carl.zhang@intel.com>

--- media_driver/linux/common/os/i915/include/uapi/drm.h
+++ media_driver/linux/common/os/i915/include/uapi/drm.h
@@ -36,13 +36,7 @@
 #ifndef _DRM_H_
 #define _DRM_H_
 
-#if defined(__KERNEL__)
-
-#include <linux/types.h>
-#include <asm/ioctl.h>
-typedef unsigned int drm_handle_t;
-
-#elif defined(__linux__)
+#if   defined(__linux__)
 
 #include <linux/types.h>
 #include <asm/ioctl.h>
@@ -141,11 +135,11 @@ struct drm_version {
 	int version_minor;	  /**< Minor version */
 	int version_patchlevel;	  /**< Patch level */
 	__kernel_size_t name_len;	  /**< Length of name buffer */
-	char __user *name;	  /**< Name of driver */
+	char *name;	  /**< Name of driver */
 	__kernel_size_t date_len;	  /**< Length of date buffer */
-	char __user *date;	  /**< User-space buffer to hold date */
+	char *date;	  /**< User-space buffer to hold date */
 	__kernel_size_t desc_len;	  /**< Length of desc buffer */
-	char __user *desc;	  /**< User-space buffer to hold desc */
+	char *desc;	  /**< User-space buffer to hold desc */
 };
 
 /**
@@ -155,12 +149,12 @@ struct drm_version {
  */
 struct drm_unique {
 	__kernel_size_t unique_len;	  /**< Length of unique */
-	char __user *unique;	  /**< Unique name for driver instantiation */
+	char *unique;	  /**< Unique name for driver instantiation */
 };
 
 struct drm_list {
 	int count;		  /**< Length of user-space structures */
-	struct drm_version __user *version;
+	struct drm_version *version;
 };
 
 struct drm_block {
@@ -355,7 +349,7 @@ struct drm_buf_desc {
  */
 struct drm_buf_info {
 	int count;		/**< Entries in list */
-	struct drm_buf_desc __user *list;
+	struct drm_buf_desc *list;
 };
 
 /**
@@ -363,7 +357,7 @@ struct drm_buf_info {
  */
 struct drm_buf_free {
 	int count;
-	int __user *list;
+	int *list;
 };
 
 /**
@@ -375,7 +369,7 @@ struct drm_buf_pub {
 	int idx;		       /**< Index into the master buffer list */
 	int total;		       /**< Buffer size */
 	int used;		       /**< Amount of buffer in use (for DMA) */
-	void __user *address;	       /**< Address of buffer */
+	void *address;	       /**< Address of buffer */
 };
 
 /**
@@ -384,11 +378,11 @@ struct drm_buf_pub {
 struct drm_buf_map {
 	int count;		/**< Length of the buffer list */
 #ifdef __cplusplus
-	void __user *virt;
+	void *virt;
 #else
-	void __user *virtual;		/**< Mmap'd area in user-virtual */
+	void *virtual;		/**< Mmap'd area in user-virtual */
 #endif
-	struct drm_buf_pub __user *list;	/**< Buffer information */
+	struct drm_buf_pub *list;	/**< Buffer information */
 };
 
 /**
@@ -401,13 +395,13 @@ struct drm_buf_map {
 struct drm_dma {
 	int context;			  /**< Context handle */
 	int send_count;			  /**< Number of buffers to send */
-	int __user *send_indices;	  /**< List of handles to buffers */
-	int __user *send_sizes;		  /**< Lengths of data to send */
+	int *send_indices;	  /**< List of handles to buffers */
+	int *send_sizes;		  /**< Lengths of data to send */
 	enum drm_dma_flags flags;	  /**< Flags */
 	int request_count;		  /**< Number of buffers requested */
 	int request_size;		  /**< Desired size for buffers */
-	int __user *request_indices;	  /**< Buffer information */
-	int __user *request_sizes;
+	int *request_indices;	  /**< Buffer information */
+	int *request_sizes;
 	int granted_count;		  /**< Number of buffers granted */
 };
 
@@ -431,7 +425,7 @@ struct drm_ctx {
  */
 struct drm_ctx_res {
 	int count;
-	struct drm_ctx __user *contexts;
+	struct drm_ctx *contexts;
 };
 
 /**
@@ -962,7 +956,6 @@ struct drm_event_crtc_sequence {
 };
 
 /* typedef area */
-#ifndef __KERNEL__
 typedef struct drm_clip_rect drm_clip_rect_t;
 typedef struct drm_drawable_info drm_drawable_info_t;
 typedef struct drm_tex_region drm_tex_region_t;
@@ -1004,7 +997,6 @@ typedef struct drm_agp_binding drm_agp_binding_t;
 typedef struct drm_agp_info drm_agp_info_t;
 typedef struct drm_scatter_gather drm_scatter_gather_t;
 typedef struct drm_set_version drm_set_version_t;
-#endif
 
 #if defined(__cplusplus)
 }
