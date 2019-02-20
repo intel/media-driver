07748c9a66057576e02d3caac71e7256977b6e7e
hanlong1 <longfei.han@intel.com>
2019-02-20 13:23:30 +0800

    Revert "[Decode] add perf async support"
    Change-Id: I035251f764cffe0f98f3f6311cbbab1cce309e21

--- media_driver/linux/common/os/mos_os_specific.c
+++ media_driver/linux/common/os/mos_os_specific.c
@@ -5207,16 +5207,8 @@ int32_t Mos_Specific_SetCpuCacheability(
 MOS_STATUS Mos_Specific_SkipResourceSync(
     PMOS_RESOURCE               pOsResource)
 {
-    MOS_STATUS eStatus = MOS_STATUS_SUCCESS;
-
-    //---------------------------------------
-    MOS_OS_CHK_NULL(pOsResource);
-    //---------------------------------------
-
-    mos_bo_set_exec_object_async(pOsResource->bo);
-
-finish:
-    return eStatus;
+    MOS_UNUSED(pOsResource);
+    return MOS_STATUS_SUCCESS;
 }
 
 //!
