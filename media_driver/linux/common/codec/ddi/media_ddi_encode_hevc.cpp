7d3dca5f6b0a465458d4fb47e4723d6c29ca8e0b
Gu_Chenchen <chenchen.gu@intel.com>
2019-02-22 16:19:54 +0800

    Revert "[Encode] Enable QVBR"
    This reverts commit 6a22e825fe9c007a177c3308535eb89e100b0498.
    Change-Id: Ifa10317e28977392a6f42e7810c1568317e7516c

--- media_driver/linux/common/codec/ddi/media_ddi_encode_hevc.cpp
+++ media_driver/linux/common/codec/ddi/media_ddi_encode_hevc.cpp
@@ -1060,12 +1060,6 @@ VAStatus DdiEncodeHevc::ParseMiscParams(void *ptr)
         {
             seqParams->RateControlMethod = RATECONTROL_VBR;
         }
-        else if (VA_RC_QVBR & m_encodeCtx->uiRCMethod)
-        {
-            seqParams->RateControlMethod = RATECONTROL_QVBR;
-            seqParams->ICQQualityFactor = vaEncMiscParamRC->ICQ_quality_factor;
-            seqParams->MBBRC = 1;
-        }
         else
         {
             DDI_ASSERTMESSAGE("invalid RC method.");
@@ -1073,8 +1067,7 @@ VAStatus DdiEncodeHevc::ParseMiscParams(void *ptr)
         }
         if((RATECONTROL_VCM == seqParams->RateControlMethod)
             || (RATECONTROL_VBR == seqParams->RateControlMethod)
-            || (RATECONTROL_CBR == seqParams->RateControlMethod)
-            || (RATECONTROL_QVBR == seqParams->RateControlMethod))
+            || (RATECONTROL_CBR == seqParams->RateControlMethod))
         {
             seqParams->MaxBitRate    = seqParams->TargetBitRate;
             seqParams->MinBitRate    = seqParams->TargetBitRate * (2 * vaEncMiscParamRC->target_percentage - 100) / 100;
