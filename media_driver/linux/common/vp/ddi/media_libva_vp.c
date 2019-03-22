9539cf45321f85ae201df7860bccec1a7ce771bd
Scarbrough, Frank <frank.scarbrough@intel.com>
2019-03-22 16:35:13 -0700

    Revert "[VP] add linear NV12/YUY2 input support"
    This reverts commit 80e3801727522c63cd8c473ab01d4cb5605c5660.
    
    Change-Id: I58feec878abc788c0a7865fd28a2ea86c88c3340

--- media_driver/linux/common/vp/ddi/media_libva_vp.c
+++ media_driver/linux/common/vp/ddi/media_libva_vp.c
@@ -1260,18 +1260,14 @@ DdiVp_SetProcPipelineParams(
             switch (pMediaSrcSurf->format)
             {
                 case Media_Format_NV12:
-                    pVpHalSrcSurf->OsResource.YPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[0];
-                    pVpHalSrcSurf->OsResource.UPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[1];
-                    pVpHalSrcSurf->OsResource.UPlaneOffset.iYOffset       = 0;
-                    pVpHalSrcSurf->OsResource.VPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[1];
-                    pVpHalSrcSurf->OsResource.VPlaneOffset.iYOffset       = 0;
+                    pVpHalSrcSurf->YPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[0];
+                    pVpHalSrcSurf->UPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[1];
+                    pVpHalSrcSurf->VPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[1];
                     break;
                 case Media_Format_YV12:
-                    pVpHalSrcSurf->OsResource.YPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[0];
-                    pVpHalSrcSurf->OsResource.VPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[1];
-                    pVpHalSrcSurf->OsResource.VPlaneOffset.iYOffset       = 0;
-                    pVpHalSrcSurf->OsResource.UPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[2];
-                    pVpHalSrcSurf->OsResource.UPlaneOffset.iYOffset       = 0;
+                    pVpHalSrcSurf->YPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[0];
+                    pVpHalSrcSurf->VPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[1];
+                    pVpHalSrcSurf->UPlaneOffset.iSurfaceOffset = pMediaSrcSurf->pSurfDesc->uiOffsets[2];
                     break;
                 case Media_Format_A8R8G8B8:
                     break;
@@ -1281,6 +1277,7 @@ DdiVp_SetProcPipelineParams(
         }
         else
         {
+            pVpHalSrcSurf->bUsrPtr                 = false;
             pVpHalSrcSurf->OsResource.bUseGmmQuery = true;
         }
     }
@@ -3021,17 +3018,11 @@ VAStatus DdiVp_BeginPicture(
         if (pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->bUsrPtr)
         {
             pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.iPitch = pMediaTgtSurf->iPitch;
-            pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.bUseGmmQuery = false;
-        }
-        else
-        {
-            pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.bUseGmmQuery = true;
         }
     }
     else
     {
         pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->bUsrPtr = false;
-        pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.bUseGmmQuery = true;
     }
     // increase render target count
     pVpHalRenderParams->uDstCount++;
@@ -3124,17 +3115,11 @@ VAStatus DdiVp_BeginPictureInt(
         if (pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->bUsrPtr)
         {
             pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.iPitch = pMediaTgtSurf->iPitch;
-            pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.bUseGmmQuery = false;
-        }
-        else
-        {
-            pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.bUseGmmQuery = true;
         }
     }
     else
     {
         pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->bUsrPtr = false;
-        pVpHalRenderParams->pTarget[pVpHalRenderParams->uDstCount]->OsResource.bUseGmmQuery = true;
     }
     // increase render target count
     pVpHalRenderParams->uDstCount++;
