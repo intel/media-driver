12febda2b413d4780ca9c39712c8d588d2a0aea6
Badiuzzaman Iskhandar <badiuzzaman.azzarfan.bin.iskhandar@intel.com>
2019-04-02 18:51:38 +0800

    Revert "MOS_CreateSemaphore: check allocatiion result and free mem if sem_init failed" - upstreamed
    This reverts commit 2e688715531469f22b7b1927559739cd182d8b8d.

--- media_driver/linux/common/os/mos_utilities_specific.c
+++ media_driver/linux/common/os/mos_utilities_specific.c
@@ -2705,11 +2705,8 @@ PMOS_SEMAPHORE MOS_CreateSemaphore(
     MOS_UNUSED(uiMaximumCount);
 
     pSemaphore = (PMOS_SEMAPHORE)MOS_AllocMemory(sizeof(*pSemaphore));
-    if (!pSemaphore)
-        return NULL;
     if (sem_init(pSemaphore, 0, uiInitialCount))
     {
-        MOS_SafeFreeMemory(pSemaphore);
         pSemaphore = nullptr;
     }
 
