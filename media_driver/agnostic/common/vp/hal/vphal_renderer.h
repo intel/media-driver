3ac89218e770c2a0441765bc404dfa8236081fad
furongzh <furong.zhang@intel.com>
2019-05-03 11:45:38 +0800

    [VP] Update Render GPU Context
    Change-Id: I06b09e8cceff46ab482ff795e01788436893aecf

--- media_driver/agnostic/common/vp/hal/vphal_renderer.h
+++ media_driver/agnostic/common/vp/hal/vphal_renderer.h
@@ -290,10 +290,11 @@ public:
     //!
     //! \brief    Update Render Gpu Context
     //! \details  Update Render Gpu Context
+    //! \param    [in] renderGpuContext
     //! \return   MOS_STATUS
     //!           Return MOS_STATUS_SUCCESS if successful, otherwise failed
     //!
-    virtual MOS_STATUS UpdateRenderGpuContext();
+    virtual MOS_STATUS UpdateRenderGpuContext(MOS_GPU_CONTEXT renderGpuContext);
 
 protected:
     //!
