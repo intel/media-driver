c002948db81fa979c2153ab5b0484bc35917b916
HeFan <fan.f.he@intel.com>
2019-05-28 13:10:42 +0800

    [CM] Add parameters to set mocs for indirect objects and stateless
    dataport access
    [CM] Add parameters to set mocs for indirect objects and stateless
    dataport access
    
    Change-Id: Ie62968b02028de2a59f04b5218e3cef4176580c7

--- media_driver/agnostic/common/hw/mhw_render.h
+++ media_driver/agnostic/common/hw/mhw_render.h
@@ -135,6 +135,8 @@ typedef struct _MHW_STATE_BASE_ADDR_PARAMS
     uint32_t                mocs4GeneralState;
     uint32_t                mocs4DynamicState;
     uint32_t                mocs4SurfaceState;
+    uint32_t                mocs4IndirectObjectBuffer;
+    uint32_t                mocs4StatelessDataport;
 } MHW_STATE_BASE_ADDR_PARAMS, *PMHW_STATE_BASE_ADDR_PARAMS;
 
 typedef struct _MHW_VFE_SCOREBOARD_DELTA
