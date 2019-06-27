49b1c04ab442f30070d19a4d57dcea98518792da
XinfengZhang <carl.zhang@intel.com>
2019-06-27 10:36:54 -0400

    convert Qmatrix of Jpeg from Zigzag to Raster order
    Signed-off-by: XinfengZhang <carl.zhang@intel.com>

--- media_driver/linux/common/codec/ddi/media_ddi_encode_jpeg.cpp
+++ media_driver/linux/common/codec/ddi/media_ddi_encode_jpeg.cpp
@@ -30,6 +30,18 @@
 #include "media_ddi_encode_const.h"
 #include "media_ddi_factory.h"
 
+static const uint32_t zigzag_order[64] =
+{
+    0,   1,  8, 16,  9,  2,  3, 10,
+    17, 24, 32, 25, 18, 11,  4,  5,
+    12, 19, 26, 33, 40, 48, 41, 34,
+    27, 20, 13,  6,  7, 14, 21, 28,
+    35, 42, 49, 56, 57, 50, 43, 36,
+    29, 22, 15, 23, 30, 37, 44, 51,
+    58, 59, 52, 45, 38, 31, 39, 46,
+    53, 60, 61, 54, 47, 55, 62, 63
+};
+
 extern template class MediaDdiFactoryNoArg<DdiEncodeBase>;
 static bool isEncodeJpegRegistered =
     MediaDdiFactoryNoArg<DdiEncodeBase>::RegisterCodec<DdiEncodeJpeg>(ENCODE_ID_JPEG);
@@ -654,7 +666,7 @@ VAStatus DdiEncodeJpeg::DefaultQmatrix()
                 quantValue = 255;
             }
 
-            quantMatrix->m_quantTable[qMatrixCount].m_qm[i] = (uint16_t)quantValue;
+            quantMatrix->m_quantTable[qMatrixCount].m_qm[zigzag_order[i]] = (uint16_t)quantValue;
         }
     }
 
@@ -703,7 +715,7 @@ VAStatus DdiEncodeJpeg::QualityScaleQmatrix()
                 quantValue = 255;
             }
 
-            quantMatrix->m_quantTable[qMatrixCount].m_qm[i] = (uint16_t)quantValue;
+            quantMatrix->m_quantTable[qMatrixCount].m_qm[zigzag_order[i]] = (uint16_t)quantValue;
         }
     }
 
