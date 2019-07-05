efe4979bc255f70021e5fcdb8ae6068dddb03e39
Wang_Xiaotong <xiaotong.wang@intel.com>
2019-07-05 09:50:11 +0800

    [VP] Refine oca error handler
    1 Rename "Last Oca Error" as "Oca Status".
    2 Add "Oca Status" and set it to 0 for no oca error case.
    3 Add On1stLevelBBStart for vdbox support.
    
    Change-Id: I0dd2bdb0d4716cec08f00b850e2bcc2c9765ec66

--- media_driver/agnostic/common/renderhal/hal_oca_interface.h
+++ media_driver/agnostic/common/renderhal/hal_oca_interface.h
@@ -43,7 +43,7 @@ public:
     //! \param  [in] mosContext
     //!         Reference to MOS_CONTEXT.
     //! \param  [in] gpuContextHandle
-    //!         Gpu context handle 
+    //!         Gpu context handle
     //! \param  [in] mhwMiInterface
     //!         Reference to MhwMiInterface.
     //! \param  [in] mmioRegisters
@@ -59,6 +59,31 @@ public:
     //!
     static void On1stLevelBBStart(MOS_COMMAND_BUFFER &cmdBuffer, MOS_CONTEXT &mosContext,
         uint32_t gpuContextHandle, MhwMiInterface &mhwMiInterface, MHW_MI_MMIOREGISTERS &mmioRegisters,
+        uint32_t offsetOf1stLevelBB = 0, bool bUseSizeOfCmdBuf = true, uint32_t sizeOf1stLevelBB = 0);
+
+    //!
+    //! \brief  Oca operation which should be called at the beginning of 1st level batch buffer start.
+    //! \param  [in/out] cmdBuffer
+    //!         Command buffer for current BB. hOcaBuf in cmdBuffer will be updated.
+    //! \param  [in] mosContext
+    //!         Reference to MOS_CONTEXT.
+    //! \param  [in] gpuContextHandle
+    //!         Gpu context handle
+    //! \param  [in] mhwMiInterface
+    //!         Reference to MhwMiInterface.
+    //! \param  [in] mmioRegisters
+    //!         mmio registers for current engine.
+    //! \param  [in] offsetOf1stLevelBB
+    //!         Offset for current BB in cmdBuffer.
+    //! \param  [in] bUseSizeOfCmdBuf
+    //!         If true, use size of cmdBuffer for batch buffer, else use sizeOf1stLevelBB.
+    //! \param  [in] sizeOf1stLevelBB
+    //!         Size of BB. Ignore if bUseSizeOfResource == true.
+    //! \return void
+    //!         No return value. Handle all exception inside the function.
+    //!
+    static void On1stLevelBBStart(MOS_COMMAND_BUFFER &cmdBuffer, MOS_CONTEXT &mosContext,
+        uint32_t gpuContextHandle, MhwMiInterface &mhwMiInterface, MmioRegistersMfx &mmioRegisters,
         uint32_t offsetOf1stLevelBB = 0, bool bUseSizeOfCmdBuf = true, uint32_t sizeOf1stLevelBB = 0);
 
     //!
