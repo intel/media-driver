3c121781fd0c2361063e744c9f73ac00de02224d
Bai_Guangyao <guangyao.bai@intel.com>
2019-07-05 13:49:42 +0800

    [VP] Fix Doxygen issue in decompresion code path
    Fix Doxygen issue in decompresion code path: add comments for new added params
    
    Change-Id: I368100c05d6720ac730ebcb8918a7c2b87d5a787

--- media_driver/agnostic/common/shared/mediamemdecomp.h
+++ media_driver/agnostic/common/shared/mediamemdecomp.h
@@ -63,9 +63,11 @@ public:
     //! \brief    Media memory decompression
     //! \details  Entry point to decompress media memory
     //! \param    [in] inputSurface
-    //!            The surface resource will be decompressed
+    //!            The source surface resource
     //! \param    [out] outputSurface
-    //!            The target uncompressed surface resource will be copied to
+    //!            The target surface resource will be copied to
+    //! \param    [in] bOutputCompressed
+    //!            true means apply compression on output surface, else output uncompressed surface
     //!
     //! \return   MOS_STATUS_SUCCESS if succeeded, else error code.
     //!
