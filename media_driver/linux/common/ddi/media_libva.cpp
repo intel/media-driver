5549a55dc8e478d571e72339a1ecc848615ab05f
Harish Krupo <harishkrupo@gmail.com>
2019-07-26 12:26:04 +0530

    media_libva: Return P010 as a composite format
    Exporting P010 buffers fails because we don't recognize it as a
    composite object. Add DRM_FORMAT_P010 as a return value for
    VA_FOURCC_P010.
    
    Signed-off-by: Harish Krupo <harishkrupo@gmail.com>

--- media_driver/linux/common/ddi/media_libva.cpp
+++ media_driver/linux/common/ddi/media_libva.cpp
@@ -5931,6 +5931,7 @@ static uint32_t DdiMedia_GetDrmFormatOfCompositeObject(uint32_t fourcc)
     case VA_FOURCC_Y800:
         return DRM_FORMAT_R8;
     case VA_FOURCC_P010:
+        return DRM_FORMAT_P010;
     case VA_FOURCC_I010:
         // These currently have no composite DRM format - they are usable
         // only as separate planes.
