6b7c51b6591d9900bbe1b6dae912003e1880fc2c
Chen_JasonK <jason.k.chen@intel.com>
2019-08-09 09:15:01 +0800

    Revert "[VP] Change output pipe to Render for Vebox disabled but procamp enabled"
    This reverts commit fa5f4e24.
    Change-Id: I12aefdbd1e1a2332a74db3cebebbcd911f6a803d

--- media_driver/agnostic/common/vp/hal/vphal_render_vebox_base.cpp
+++ media_driver/agnostic/common/vp/hal/vphal_render_vebox_base.cpp
@@ -4576,8 +4576,7 @@ bool VPHAL_VEBOX_STATE::IS_COMP_BYPASS_FEASIBLE(bool _bCompNeeded, PCVPHAL_RENDE
 bool VPHAL_VEBOX_STATE::IS_OUTPUT_PIPE_VEBOX_FEASIBLE(PVPHAL_VEBOX_STATE _pVeboxState, PCVPHAL_RENDER_PARAMS _pcRenderParams, PVPHAL_SURFACE _pSrcSurface)
 {
     VPHAL_RENDER_NORMALMESSAGE(
-        "FtrDisableVEBoxFeatures %d,                                                        \
-         dwCompBypassMode %d,                                                               \
+        "dwCompBypassMode %d,                                                               \
          _pcRenderParams->uDstCount %d,                                                     \
          SAME_SIZE_RECT(rcSrc, rcDst) %d,                                                   \
          SAME_SIZE_RECT(rcSrc, rcMaxSrc) %d,                                                \
@@ -4592,7 +4591,6 @@ bool VPHAL_VEBOX_STATE::IS_OUTPUT_PIPE_VEBOX_FEASIBLE(PVPHAL_VEBOX_STATE _pVebox
          AlphaMode %p,                                                                      \
          rcDst.top %p,                                                                      \
          rcDst.left %p",
-        MEDIA_IS_SKU(m_pSkuTable, FtrDisableVEBoxFeatures),
         _pVeboxState->dwCompBypassMode,
         _pcRenderParams->uDstCount,
         SAME_SIZE_RECT(_pSrcSurface->rcSrc, _pSrcSurface->rcDst),
@@ -4609,8 +4607,7 @@ bool VPHAL_VEBOX_STATE::IS_OUTPUT_PIPE_VEBOX_FEASIBLE(PVPHAL_VEBOX_STATE _pVebox
         _pSrcSurface->rcDst.top,
         _pSrcSurface->rcDst.left);
 
-    return (!MEDIA_IS_SKU(m_pSkuTable, FtrDisableVEBoxFeatures) &&
-            _pVeboxState->dwCompBypassMode != VPHAL_COMP_BYPASS_DISABLED &&
+    return (_pVeboxState->dwCompBypassMode != VPHAL_COMP_BYPASS_DISABLED &&
             _pcRenderParams->uDstCount == 1 &&
             SAME_SIZE_RECT(_pSrcSurface->rcSrc, _pSrcSurface->rcDst) &&
             SAME_SIZE_RECT(_pSrcSurface->rcSrc, _pSrcSurface->rcMaxSrc) &&
