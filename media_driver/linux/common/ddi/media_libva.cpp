7dbc19b85098dd5741dcb60f54c2fc54141d6232
XiaogangLi <xiaogang.li@intel.com>
2019-09-19 19:50:35 +0800

    [VP] Fix ABGR10 regression for VP
    Fix ABGR10 regression which introduced by commit dc756c3dc1bdc41ef0a032c5eb9651adf3efd864.
    
    Change-Id: Ide125055326933a5ab419ac794255cc3d3337745

--- media_driver/linux/common/ddi/media_libva.cpp
+++ media_driver/linux/common/ddi/media_libva.cpp
@@ -401,8 +401,9 @@ int32_t DdiMedia_MediaFormatToOsFormat(DDI_MEDIA_FORMAT format)
         case Media_Format_X8B8G8R8:
             return VA_FOURCC_XBGR;
         case Media_Format_A8B8G8R8:
-        case Media_Format_R10G10B10A2:
             return VA_FOURCC_ABGR;
+        case Media_Format_R10G10B10A2:
+            return VA_FOURCC_A2B10G10R10;
         case Media_Format_R8G8B8A8:
             return VA_FOURCC_RGBA;
         case Media_Format_A8R8G8B8:
