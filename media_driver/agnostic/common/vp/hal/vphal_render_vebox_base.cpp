cf011904080ca0b145350ea02f21c4908dfd8e3b
Jia_LinA <lin.a.jia@intel.com>
2019-09-26 17:10:22 +0800

    [VP] Fix klockwork issues
    Fix klockwork error issues
    
    Change-Id: I83d1a835ed6c44f0bb7ea2eaab6f5b1a8451a8c2

--- media_driver/agnostic/common/vp/hal/vphal_render_vebox_base.cpp
+++ media_driver/agnostic/common/vp/hal/vphal_render_vebox_base.cpp
@@ -3272,7 +3272,7 @@ MOS_STATUS VPHAL_VEBOX_STATE::VeboxRenderMode0(
     // Set up reference surfaces
     pRefSurface = VeboxSetReference(pSrcSurface);
 
-    if (pSrcSurface->bPreAPGWorkloadEnable)
+    if (pSrcSurface->bPreAPGWorkloadEnable && pRefSurface != nullptr)
     {
         pRefSurface->bPreAPGWorkloadEnable = false;
         pRenderData->bRefValid = false;
