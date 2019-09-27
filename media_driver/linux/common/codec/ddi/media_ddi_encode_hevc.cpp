8ac4fb0aa57677cdc573c9e5f39976600319be9e
Han_Longfei <longfei.han@intel.com>
2019-09-27 18:47:31 +0800

    Revert "[Encode] Fix Clipping issue for Min/Max QP feature"
    This reverts commit 2f25a087.
    
    Change-Id: I744af6cdeef696e4fb41a3d5097491c6a8c6cee8

--- media_driver/linux/common/codec/ddi/media_ddi_encode_hevc.cpp
+++ media_driver/linux/common/codec/ddi/media_ddi_encode_hevc.cpp
@@ -1062,16 +1062,8 @@ VAStatus DdiEncodeHevc::ParseMiscParams(void *ptr)
         //enable parallelBRC for Android and Linux
         seqParams->ParallelBRC = vaEncMiscParamRC->rc_flags.bits.enable_parallel_brc;
 
-        if (true == m_encodeCtx->bVdencActive)
-        {
-            picParams->BRCMaxQp  = (vaEncMiscParamRC->max_qp == 0) ? 51 : (uint8_t)CodecHal_Clip3(1, 51, (int8_t)vaEncMiscParamRC->max_qp);
-            picParams->BRCMinQp = (vaEncMiscParamRC->min_qp == 0) ? 1 : (uint8_t)CodecHal_Clip3(1, picParams->BRCMaxQp, (int8_t)vaEncMiscParamRC->min_qp);
-        }
-        else
-        {
-            picParams->BRCMaxQp  = (uint8_t)CodecHal_Clip3(0, 51, (int8_t)vaEncMiscParamRC->max_qp); //use 0 to ignore
-            picParams->BRCMinQp = (uint8_t)CodecHal_Clip3(0, picParams->BRCMaxQp, (int8_t)vaEncMiscParamRC->min_qp); //use 0 to ignore
-        }
+        picParams->BRCMaxQp  = (vaEncMiscParamRC->max_qp == 0) ? 51 : (uint8_t)CodecHal_Clip3(1, 51, (int8_t)vaEncMiscParamRC->max_qp);
+        picParams->BRCMinQp = (vaEncMiscParamRC->min_qp == 0) ? 1 : (uint8_t)CodecHal_Clip3(1, picParams->BRCMaxQp, (int8_t)vaEncMiscParamRC->min_qp);
 
         if (VA_RC_NONE == m_encodeCtx->uiRCMethod || VA_RC_CQP == m_encodeCtx->uiRCMethod)
         {
