7e125fcbac17773b03a4d4009c7da2024c0528f0
Jia_LinA <lin.a.jia@intel.com>
2019-10-09 16:45:52 +0800

    [VP] LACE feature enable
    LACE kernel integration and computer walker enable
    
    Change-Id: Ifab05b076047b7f8a56d80c9655573d56ebd53b9

--- media_driver/agnostic/common/vp/hal/vphal_render_vebox_base.cpp
+++ media_driver/agnostic/common/vp/hal/vphal_render_vebox_base.cpp
@@ -4942,6 +4942,7 @@ MOS_STATUS VPHAL_VEBOX_RENDER_DATA::Init()
     }
     pDNUVParams          = nullptr;
     iCurbeLength         = 0;
+    iCurbeOffset         = 0;
     iInlineLength        = 0;
     // Debug parameters
     pKernelName          = nullptr;
