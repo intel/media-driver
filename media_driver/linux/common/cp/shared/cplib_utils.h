efcbb29af835081943795b177e57387c1e655bea
jingyua1 <vicky.j.liu@intel.com>
2019-12-19 08:57:33 +0800

    [CP] enable hucstreamoutout path of hevc PR decode
    enable hucstreamoutout path of hevc PR decode.
    generate a subpipeline class to allocate resources and prepare
    hucStreamOut's params.
    generate a cpstreamout pkt.
    
    Change-Id: I30dd15e0c4519ae7bb22b0834b4d1efaad607a07

--- media_driver/linux/common/cp/shared/cplib_utils.h
+++ media_driver/linux/common/cp/shared/cplib_utils.h
@@ -102,6 +102,8 @@ public:
     static const char* FUNC_DELETE_SECUREDECODE;
     static const char* FUNC_CREATE_DECODECP;
     static const char* FUNC_DELETE_DECODECP;
+    static const char* FUNC_CREATE_CPSTREAMOUT;
+    static const char* FUNC_DELETE_CPSTREAMOUT;
     static const char* FUNC_SETUP_MOS_APO_SWITCH;
 
     static std::unordered_map<const char*, void*> m_symbols;
