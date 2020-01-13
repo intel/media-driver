ead8f5602f99c0bd13c753ae43be0d92ec58f0db
Han_Longfei <longfei.han@intel.com>
2020-01-13 19:20:24 +0800

    Revert "[CM] cmrtlib drm_device.h update Linux Open Source"
    This reverts commit 976e7d4f.
    Change-Id: I31f2d443083abb3c9cb19a9e98f5bbd897d4ec07

--- cmrtlib/linux/hardware/drm_device.h
+++ cmrtlib/linux/hardware/drm_device.h
@@ -519,7 +519,7 @@ static int parse_separate_sysfs_files(int maj, int min,
         "subsystem_vendor",
         "subsystem_device",
     };
-    char path[PATH_MAX + 1], pci_path[PATH_MAX -256];
+    char path[PATH_MAX + 1], pci_path[PATH_MAX + 1];
     char resourcename[PATH_MAX + 1], driverpath[PATH_MAX + 1], drivername[PATH_MAX + 1], irqpath[PATH_MAX + 1];
 
     unsigned int data[ARRAY_SIZE(attrs)];
