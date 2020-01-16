aefbf6c35178ed956076751735ff37f0f92d5035
dcbrown <david.c.brown@intel.com>
2020-01-16 10:45:09 +0800

    [Encode] HEVC VME Panic Mode PAK fix
    Panic Mode Pak Pass lcu x/y position and prediction fix
    
    Change-Id: If76cfbcdce4296ba5d1d2ad27d353d77e40f9fcd

--- media_driver/agnostic/gen12/codec/hal/codechal_encode_hevc_g12.cpp
+++ media_driver/agnostic/gen12/codec/hal/codechal_encode_hevc_g12.cpp
@@ -6120,10 +6120,29 @@ MOS_STATUS CodechalEncHevcStateG12::GenerateSkipFrameMbCodeSurface(SkipFrameInfo
     auto pakObjData = (HcpPakObjectG12 *)data;
     auto cuData     = (EncodeHevcCuDataG12 *)(data + m_mvOffset);
 
-    auto const ctbSize        = 1 << (m_hevcSeqParams->log2_max_coding_block_size_minus3 + 3);
-    auto const maxNumCuInCtb  = (ctbSize / CODECHAL_HEVC_MIN_CU_SIZE) * (ctbSize / CODECHAL_HEVC_MIN_CU_SIZE);
-    auto const picWidthInCtb  = MOS_ROUNDUP_DIVIDE(m_frameWidth, ctbSize);
-    auto const picHeightInCtb = MOS_ROUNDUP_DIVIDE(m_frameHeight, ctbSize);
+    auto const ctbSize          = 1 << (m_hevcSeqParams->log2_max_coding_block_size_minus3 + 3);
+    auto const maxNumCuInCtb    = (ctbSize / CODECHAL_HEVC_MIN_CU_SIZE) * (ctbSize / CODECHAL_HEVC_MIN_CU_SIZE);
+    auto const picWidthInCtb    = MOS_ROUNDUP_DIVIDE(m_frameWidth, ctbSize);
+    auto const picHeightInCtb   = MOS_ROUNDUP_DIVIDE(m_frameHeight, ctbSize);
+    uint32_t   num_tile_columns = m_hevcPicParams->num_tile_columns_minus1 + 1;
+    uint32_t * tileColumnsStartPosition{new uint32_t[num_tile_columns]{}};
+
+    for (uint32_t i = 0; i < (num_tile_columns); i++)
+    {
+        if (m_hevcPicParams->tile_column_width[i] == 0)
+        {
+            tileColumnsStartPosition[i] = picWidthInCtb;
+            break;
+        }
+
+        if (i == 0)
+        {
+            tileColumnsStartPosition[i] = m_hevcPicParams->tile_column_width[i];
+            continue;
+        }
+
+        tileColumnsStartPosition[i] = tileColumnsStartPosition[i - 1] + m_hevcPicParams->tile_column_width[i];
+    }
 
     // Prepare CTB splits for corner cases:
     // Last column
@@ -6142,14 +6161,33 @@ MOS_STATUS CodechalEncHevcStateG12::GenerateSkipFrameMbCodeSurface(SkipFrameInfo
     uint32_t ctbXAddr;
     uint32_t ctbYAddr;
     uint32_t nCUs;
+    uint32_t tileEnd;
+    uint32_t tileStart;
     for (uint32_t slcIdx = 0; slcIdx < m_numSlices; ++slcIdx)
     {
         sliceFirstCtbIdx = m_hevcSliceParams[slcIdx].slice_segment_address;
-        for (uint32_t ctbIdxInSlice = 0; ctbIdxInSlice < m_hevcSliceParams[slcIdx].NumLCUsInSlice; ++ctbIdxInSlice, ++pakObjData)
+        tileEnd          = 0;
+        tileStart        = 0;
+        ctbXAddr         = sliceFirstCtbIdx % picWidthInCtb;
+        ctbYAddr         = sliceFirstCtbIdx / picWidthInCtb;
+        for (uint32_t i = 0; i < num_tile_columns; i++)
         {
-            ctbXAddr = (sliceFirstCtbIdx + ctbIdxInSlice) % picWidthInCtb;
-            ctbYAddr = (sliceFirstCtbIdx + ctbIdxInSlice) / picWidthInCtb;
+            //Determine what tile slice belongs to
+            if (ctbXAddr < tileColumnsStartPosition[i])
+            {
+                tileEnd   = tileColumnsStartPosition[i];
+                tileStart = (i == 0) ? 0 : tileColumnsStartPosition[i - 1];
+                break;
+            }
+        }
 
+        for (uint32_t ctbIdxInSlice = 0; ctbIdxInSlice < m_hevcSliceParams[slcIdx].NumLCUsInSlice; ++ctbIdxInSlice, ++pakObjData, ++ctbXAddr)
+        {
+            if (ctbXAddr >= tileEnd)
+            {
+                ctbYAddr++;
+                ctbXAddr = tileStart;
+            }
             pakObjData->DW0.Type                    = 0x03;
             pakObjData->DW0.Opcode                  = 0x27;
             pakObjData->DW0.SubOp                   = 0x21;
@@ -6203,8 +6241,9 @@ MOS_STATUS CodechalEncHevcStateG12::GenerateSkipFrameMbCodeSurface(SkipFrameInfo
 
                 // Note that this can work only for B slices.
                 // If P slice support appears, we need to have the 2nd skipFrameMbCodeSurface
-                cuData->DW7_InterPredIdcMv0 = 2;  // bidirectional
-                cuData->DW7_InterPredIdcMv1 = 2;  // bidirectional
+                // When panic mode is triggered backwards reference only should be used
+                cuData->DW7_InterPredIdcMv0 = 0;
+                cuData->DW7_InterPredIdcMv1 = 0;
 
                 if (bCtbCrossRightBottomPicBoundary)
                 {
@@ -6242,11 +6281,25 @@ MOS_STATUS CodechalEncHevcStateG12::GenerateSkipFrameMbCodeSurface(SkipFrameInfo
                 }
             }
             cuData += (maxNumCuInCtb - nCUs);  // Shift to CUs of next CTB
+
+
         }
     }
     m_osInterface->pfnUnlockResource(m_osInterface, &skipframeInfo.m_resMbCodeSkipFrameSurface);
+    delete[] tileColumnsStartPosition;
 
     skipframeInfo.numSlices = m_numSlices;
+    uint32_t mbCodeSize     = m_mbCodeSize + 8 * CODECHAL_CACHELINE_SIZE;
+
+    #if USE_CODECHAL_DEBUG_TOOL
+    CODECHAL_ENCODE_CHK_STATUS_RETURN(m_debugInterface->DumpBuffer(
+        &skipframeInfo.m_resMbCodeSkipFrameSurface,
+        CodechalDbgAttr::attrInput,
+        "SkipFrameSurface",
+        mbCodeSize,
+        0,
+        CODECHAL_MEDIA_STATE_BRC_UPDATE));
+    #endif
 
     return eStatus;
 }
