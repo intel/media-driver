11fd7a1d0fa5dd8ca00fd3241f3364279e8d5251
Linjie Fu <linjie.fu@intel.com>
2020-02-11 14:27:29 +0800

    media_libva_caps: add VAConfigAttribEncSliceStructure caps for VAEntrypointEncSliceLP
    Add VA_ENC_SLICE_STRUCTURE_ARBITRARY_ROWS for VAEntrypointEncSliceLP.
    
    Fix #805.
    
    Signed-off-by: Linjie Fu <linjie.fu@intel.com>

--- media_driver/linux/common/ddi/media_libva_caps.cpp
+++ media_driver/linux/common/ddi/media_libva_caps.cpp
@@ -622,7 +622,8 @@ VAStatus MediaLibvaCaps::CreateEncAttributes(
     attrib.type = VAConfigAttribEncSliceStructure;
     if (entrypoint == VAEntrypointEncSliceLP)
     {
-        attrib.value = VA_ENC_SLICE_STRUCTURE_EQUAL_ROWS | VA_ENC_SLICE_STRUCTURE_MAX_SLICE_SIZE;
+        attrib.value = VA_ENC_SLICE_STRUCTURE_EQUAL_ROWS | VA_ENC_SLICE_STRUCTURE_MAX_SLICE_SIZE |
+                       VA_ENC_SLICE_STRUCTURE_ARBITRARY_ROWS;
     }
     else
     {
