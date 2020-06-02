0ea4a7cc55dc2745a48e5ac3458e3099ae057384
Sudarshan S <sudarshan.s@intel.com>
2020-06-02 06:16:37 +0530

    Compilation errors with clang
    Fix compilation error in Debug build with clang-11 enabled.

--- media_driver/agnostic/gen10/renderhal/renderhal_g10.cpp
+++ media_driver/agnostic/gen10/renderhal/renderhal_g10.cpp
@@ -133,7 +133,7 @@ MOS_STATUS XRenderHal_Interface_g10::SetupSurfaceState (
     MHW_RENDERHAL_CHK_NULL(pRenderHal->pStateHeap);
     MHW_RENDERHAL_CHK_NULL(pRenderHal->pHwSizes);
     MHW_RENDERHAL_CHK_NULL(pRenderHal->pMhwStateHeap);
-    MHW_RENDERHAL_ASSERT(pRenderHalSurface->Rotation >= 0 &&
+    MHW_RENDERHAL_ASSERT(pRenderHalSurface->Rotation >= MHW_ROTATION_IDENTITY &&
                          pRenderHalSurface->Rotation <= MHW_ROTATE_90_MIRROR_HORIZONTAL);
     //-----------------------------------------
 
