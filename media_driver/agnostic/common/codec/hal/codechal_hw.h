7b15fd50f7bd1f28570e210f5149f15cd1e19a44
XuZhengguo <zhengguo.xu@intel.com>
2021-02-01 16:06:54 +0800

    [Decode] Update filmgrain kernel
    Update filmgrain kernel

--- media_driver/agnostic/common/codec/hal/codechal_hw.h
+++ media_driver/agnostic/common/codec/hal/codechal_hw.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2011-2020, Intel Corporation
+* Copyright (c) 2011-2021, Intel Corporation
 *
 * Permission is hereby granted, free of charge, to any person obtaining a
 * copy of this software and associated documentation files (the "Software"),
@@ -1569,6 +1569,11 @@ public:
     }
 
     virtual bool UsesRenderEngine(CODECHAL_FUNCTION codecFunction, uint32_t standard);
+
+    virtual bool Uses2PlanesInputSurfaceFilmGrain()
+    {
+        return false;
+    }
 
     //!
     //! \brief    Get film grain kernel info
