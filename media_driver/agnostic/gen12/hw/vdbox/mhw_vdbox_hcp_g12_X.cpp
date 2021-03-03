3d57f08458563b7e48ffdf1ef9e3868d24e0a78c
weizhu-intel <wei1.zhu@intel.com>
2021-03-03 11:57:31 +0800

    Increase patchlist buffer for MI_STORE_DATA_IMM_CMD
    For real scalibility case, 15 MI_STORE_DATA_IMM_CMDs are added for BE
    sync, but Patchlist size is not added.

--- media_driver/agnostic/gen12/hw/vdbox/mhw_vdbox_hcp_g12_X.cpp
+++ media_driver/agnostic/gen12/hw/vdbox/mhw_vdbox_hcp_g12_X.cpp
@@ -713,7 +713,7 @@ MOS_STATUS MhwVdboxHcpInterfaceG12::GetHcpStateCommandSize(
                     4 * PATCH_LIST_COMMAND(MI_ATOMIC_CMD) +
                     2 * PATCH_LIST_COMMAND(MI_CONDITIONAL_BATCH_BUFFER_END_CMD) +
                     3 * PATCH_LIST_COMMAND(MI_SEMAPHORE_WAIT_CMD) +
-                    3 * PATCH_LIST_COMMAND(MI_STORE_DATA_IMM_CMD) +
+                    18 * PATCH_LIST_COMMAND(MI_STORE_DATA_IMM_CMD) +
                     2 * PATCH_LIST_COMMAND(MI_FLUSH_DW_CMD) +
                     2 * PATCH_LIST_COMMAND(MI_STORE_REGISTER_MEM_CMD);
 
