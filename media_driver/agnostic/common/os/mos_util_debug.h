45479f584437313c5f99b25b817aa83974dd5732
luleilei1 <leileix.lu@intel.com>
2021-03-15 16:04:13 +0800

    Revert "[Media Interface] cmd object macro update"
    Reverts intel-innersource/drivers.gpu.unified#14533

--- media_driver/agnostic/common/os/mos_util_debug.h
+++ media_driver/agnostic/common/os/mos_util_debug.h
@@ -653,20 +653,6 @@ void _MOS_Assert(
 }
 
 //!
-//! \def MOS_CHK_HR_NO_STATUS_RETURN(_compID, _subCompID, _stmt)
-//!  Check _stmt, assert and return void
-//!
-#define MOS_CHK_HR_NO_STATUS_RETURN(_compID, _subCompID, _stmt)                             \
-{                                                                                           \
-    hr = (_stmt);                                                                           \
-    if (hr != MOS_STATUS_SUCCESS)                                                           \
-    {                                                                                       \
-        MOS_ASSERTMESSAGE(_compID, _subCompID, "MOS returned error, hr = 0x%x", hr);        \
-        return;                                                                             \
-    }                                                                                       \
-}
-
-//!
 //! \def MOS_CHK_STATUS_MESSAGE(_compID, _subCompID, _stmt, _message, ...)
 //!  Check MOS_STATUS \a _stmt, assert and return an error for failure, and print message
 //!
