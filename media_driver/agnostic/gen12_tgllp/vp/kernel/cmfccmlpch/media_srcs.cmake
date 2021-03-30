b23a66774d00f65109c9e68c41598cffe7c13fc1
LiXin <xin6.li@intel.com>
2021-03-30 09:06:05 +0800

    [CP] stout vp kernel integration on gen12 gen9 mixed platform
    stout vp kernel integration on gen12 cpu + gen9_cml pch platform, the kernel is using Gen9_cml key

--- media_driver/agnostic/gen12_tgllp/vp/kernel/cmfccmlpch/media_srcs.cmake
+++ media_driver/agnostic/gen12_tgllp/vp/kernel/cmfccmlpch/media_srcs.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2019-2021, Intel Corporation
+# Copyright (c) 2021, Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -18,37 +18,33 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-#check whether need some flags to guard the including of below folders
-media_include_subdirectory(cmfc)
-media_include_subdirectory(cmfcpatch)
-media_include_subdirectory(cmfccmlpch)
-
-set(TMP_1_SOURCES_
-    ${CMAKE_CURRENT_LIST_DIR}/igvpkrn_isa_g12_tgllp.c
+set(TMP_SOURCES_
+    ${CMAKE_CURRENT_LIST_DIR}/igvpkrn_g12_tgllp_cmfccmlpch.c
 )
 
-set(TMP_1_HEADERS_
-    ${CMAKE_CURRENT_LIST_DIR}/igvpkrn_isa_g12_tgllp.h
+
+set(TMP_HEADERS_
+    ${CMAKE_CURRENT_LIST_DIR}/igvpkrn_g12_tgllp_cmfccmlpch.h
 )
 
 set(SOURCES_
     ${SOURCES_}
-    ${TMP_1_SOURCES_}
+    ${TMP_SOURCES_}
 )
 
 set(HEADERS_
     ${HEADERS_}
-    ${TMP_1_HEADERS_}
+    ${TMP_HEADERS_}
 )
 
 set(COMMON_SOURCES_
     ${COMMON_SOURCES_}
-    ${TMP_1_SOURCES_}
+    ${TMP_SOURCES_}
 )
 
 set(COMMON_HEADERS_
     ${COMMON_HEADERS_}
-    ${TMP_1_HEADERS_}
+    ${TMP_HEADERS_}
 )
 
 source_group( "Kernel\\VpKernel" FILES ${TMP_SOURCES_} ${TMP_HEADERS_} )
