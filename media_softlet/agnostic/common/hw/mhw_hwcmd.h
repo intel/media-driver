e359ff8818588dc686fd4b56f9721aea74cdb3e2
walter-bai <walter.bai@intel.com>
2021-06-16 11:59:01 +0800

    [Encode] Refactor MHW cmd structs
    Refactor MHW cmd structs.

---
+++ media_softlet/agnostic/common/hw/mhw_hwcmd.h
@@ -0,0 +1,40 @@
+/*
+* Copyright (c) 2021, Intel Corporation
+*
+* Permission is hereby granted, free of charge, to any person obtaining a
+* copy of this software and associated documentation files (the "Software"),
+* to deal in the Software without restriction, including without limitation
+* the rights to use, copy, modify, merge, publish, distribute, sublicense,
+* and/or sell copies of the Software, and to permit persons to whom the
+* Software is furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+* ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+* OTHER DEALINGS IN THE SOFTWARE.
+*/
+//!
+//! \file     mhw_hwcmd.h
+//! \brief    Common macros and functions to define MHW command structs.
+//!
+
+#ifndef __MHW_HWCMD_H__
+#define __MHW_HWCMD_H__
+
+#include <cstdint>
+#include <cstddef>
+
+#define __CODEGEN_MAX(_a, _b) (((_a) > (_b)) ? (_a) : (_b))
+#define __CODEGEN_BITFIELD(l, h) (h) - (l) + 1
+#define __CODEGEN_OP_LENGTH_BIAS 2
+#define __CODEGEN_OP_LENGTH(x) (uint32_t)((__CODEGEN_MAX(x, __CODEGEN_OP_LENGTH_BIAS)) - __CODEGEN_OP_LENGTH_BIAS)
+
+void MOS_ZeroMemory(void *pDestination, size_t stLength);
+
+#endif  // __MHW_HWCMD_H__
