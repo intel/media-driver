e9b4894d8395eb8d11baeae054889f2ee73bf37a
luleilei1 <leileix.lu@intel.com>
2021-07-16 13:14:53 +0800

    Revert "[Encode] Rewrite raw surface color/tile format check"
    Reverts intel-innersource/drivers.gpu.unified#31585

--- media_driver/agnostic/common/codec/hal/codechal_encoder_base.cpp
+++ media_driver/agnostic/common/codec/hal/codechal_encoder_base.cpp
@@ -1289,7 +1289,8 @@ MOS_STATUS CodechalEncoderState::CheckResChangeAndCsc()
         CODECHAL_ENCODE_CHK_STATUS_RETURN(
             m_cscDsState->CheckReconSurfaceAlignment(&m_reconSurface));
 
-        if (CodecHal_PictureIsField(m_currOriginalPic) ||
+        if (!m_cscDsState->IsEnabled() ||
+            CodecHal_PictureIsField(m_currOriginalPic) ||
             CodecHal_PictureIsInterlacedFrame(m_currOriginalPic))
         {
             // CSC disabled for interlaced frame
