6e5a2cb4f2b57ae492876538b972e1e73917bca8
xiaoton1 <xiaotong.wang@intel.com>
2021-10-22 13:28:21 +0800

    [VP] Add Kdll related code for platform in APO
    Add Kdll related code for platform in APO.

--- media_softlet/agnostic/common/vp/kdll/media_srcs.cmake
+++ media_softlet/agnostic/common/vp/kdll/media_srcs.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2018-2021, Intel Corporation
+# Copyright (c) 2021, Intel Corporation
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -18,5 +18,30 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
-media_include_subdirectory(hal)
-media_include_subdirectory(kdll)
+set(TMP_SOURCES_
+    ${CMAKE_CURRENT_LIST_DIR}/hal_kerneldll_next.c
+)
+
+set(TMP_HEADERS_ "")
+
+
+set(SOURCES_
+    ${SOURCES_}
+    ${TMP_SOURCES_}
+ )
+
+set(COMMON_SOURCES_
+    ${COMMON_SOURCES_}
+    ${TMP_SOURCES_}
+)
+
+# no header for now
+#set(HEADERS_
+#    ${HEADERS_}
+#    ${TMP_HEADERS_}
+#)
+
+source_group( "VpHalNext\\Kernel DLL" FILES ${TMP_SOURCES_} ${TMP_HEADERS_} )
+
+
+#media_add_curr_to_include_path()
