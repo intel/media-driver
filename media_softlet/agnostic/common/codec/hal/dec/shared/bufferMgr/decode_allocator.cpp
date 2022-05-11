26bab25082cc1045d0c7a71014608c989eb59910
wangqia6 <qian6.wang@intel.com>
2022-05-11 17:15:41 +0800

    [Decode] Fix regression for small lmem batch buffer setting
    For content protection, the decode batch buffer can not allocate in system memory for security

--- media_softlet/agnostic/common/codec/hal/dec/shared/bufferMgr/decode_allocator.cpp
+++ media_softlet/agnostic/common/codec/hal/dec/shared/bufferMgr/decode_allocator.cpp
@@ -223,11 +223,20 @@ PMHW_BATCH_BUFFER DecodeAllocator::AllocateBatchBuffer(
     bool notLockable = false;
     bool inSystemMem = false;
 
-    // Config setting if running with limited LMem bar config.
     if (m_limitedLMemBar)
     {
-        notLockable = (accessReq == notLockableVideoMem);
-        inSystemMem = (accessReq == lockableSystemMem);
+        // Config setting if running with limited LMem bar config with CP
+        if (m_osInterface->osCpInterface->IsCpEnabled())
+        {
+            notLockable = (accessReq == notLockableVideoMem);
+            inSystemMem = (accessReq == lockableSystemMem);
+        }
+        else
+        {
+            // allocate decode batch buffer in systemMem with limited LMem bar config due to local memory limitation
+            notLockable = false;
+            inSystemMem = true;
+        }
     }
 
 #if (_DEBUG || _RELEASE_INTERNAL)
