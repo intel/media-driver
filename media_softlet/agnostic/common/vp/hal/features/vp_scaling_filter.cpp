d1ad1f467d90f630a1a6bd2a2620f56432a61dd8
xiaoton1 <xiaotong.wang@intel.com>
2022-06-15 21:23:02 +0800

    [VP] Align vebox input resolution with sfc input resolution and disable colorfill flag for iscaling
    1. Align vebox input resolution with sfc input resolution.
    2. disable colorfill flag for iscaling.

--- media_softlet/agnostic/common/vp/hal/features/vp_scaling_filter.cpp
+++ media_softlet/agnostic/common/vp/hal/features/vp_scaling_filter.cpp
@@ -157,12 +157,22 @@ MOS_STATUS VpScalingFilter::IsColorfillEnable()
 {
     VP_FUNC_CALL();
 
-    m_bColorfillEnable = (m_scalingParams.pColorFillParams &&
-        (!m_scalingParams.pColorFillParams->bDisableColorfillinSFC) &&
-        (m_scalingParams.pColorFillParams->bOnePixelBiasinSFC ?
-        (!RECT1_CONTAINS_RECT2_ONEPIXELBIAS(m_scalingParams.input.rcDst, m_scalingParams.output.rcDst)) :
-        (!RECT1_CONTAINS_RECT2(m_scalingParams.input.rcDst, m_scalingParams.output.rcDst)))) ?
-        true : false;
+    if (ISCALING_NONE != m_scalingParams.interlacedScalingType)
+    {
+        // No colorfill for interlacedScaling.
+        // dwOutputFrameWidth/dwOutputFrameHeight will be set to dwScaledRegionWidth/dwScaledRegionHeight
+        // by SetInterlacedScalingParams
+        m_bColorfillEnable = false;
+    }
+    else
+    {
+        m_bColorfillEnable = (m_scalingParams.pColorFillParams &&
+            (!m_scalingParams.pColorFillParams->bDisableColorfillinSFC) &&
+            (m_scalingParams.pColorFillParams->bOnePixelBiasinSFC ?
+            (!RECT1_CONTAINS_RECT2_ONEPIXELBIAS(m_scalingParams.input.rcDst, m_scalingParams.output.rcDst)) :
+            (!RECT1_CONTAINS_RECT2(m_scalingParams.input.rcDst, m_scalingParams.output.rcDst)))) ?
+            true : false;
+    }
 
     return MOS_STATUS_SUCCESS;
 }
