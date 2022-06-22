d5577e88fda6c2b39be034c915cd15f0a4574e8a
JayYang <jay.yang@intel.com>
2022-06-22 16:31:47 +0800

    [VP] Add I420 and UVVY format support
    New format support in DDI.

--- media_driver/linux/common/ddi/media_libva.cpp
+++ media_driver/linux/common/ddi/media_libva.cpp
@@ -2724,6 +2724,9 @@ VAStatus DdiMedia_CreateSurfaces2(
         case VA_FOURCC_I420:
             expected_fourcc = VA_FOURCC_I420;
             break;
+        case VA_FOURCC_UYVY:
+            expected_fourcc = VA_FOURCC_UYVY;
+            break;
 #endif
         default:
             DDI_ASSERTMESSAGE("Invalid VAConfigAttribRTFormat: 0x%x. Please uses the format defined in libva/va.h", format);
@@ -4813,6 +4816,7 @@ VAStatus DdiMedia_DeriveImage (
     {
     case Media_Format_YV12:
     case Media_Format_I420:
+    case Media_Format_IYUV:
         vaimg->format.bits_per_pixel    = 12;
         vaimg->num_planes               = 3;
         vaimg->pitches[0]               = mediaSurface->iPitch;
