9f97d687f735720ab31082adf6a2dc2bdbf5fe15
JayYang <jay.yang@intel.com>
2022-07-08 14:24:51 +0800

    [Media Common] [Media Copy] Fix blt copy P010 P016 page fault
    1. P010 and P016 are 16bit format.
    2. Ensure dst bottom not exceed srs height.

--- media_driver/agnostic/common/shared/mediacopy/media_blt_copy.cpp
+++ media_driver/agnostic/common/shared/mediacopy/media_blt_copy.cpp
@@ -190,6 +190,7 @@ MOS_STATUS BltState::SetupBltCopyParam(
     BLT_CHK_NULL_RETURN(pMhwBltParams);
     BLT_CHK_NULL_RETURN(inputSurface);
     BLT_CHK_NULL_RETURN(outputSurface);
+    BLT_CHK_NULL_RETURN(outputSurface->pGmmResInfo);
 
     uint32_t          BytesPerTexel = 1;
     MOS_SURFACE       ResDetails;
@@ -199,6 +200,7 @@ MOS_STATUS BltState::SetupBltCopyParam(
     BLT_CHK_STATUS_RETURN(m_osInterface->pfnGetResourceInfo(m_osInterface, inputSurface, &ResDetails));
 
     uint32_t inputHeight = ResDetails.dwHeight;
+    uint32_t inputWidth  = ResDetails.dwWidth;
     uint32_t inputPitch  = ResDetails.dwPitch;
 
     if (inputSurface->TileType != MOS_TILE_LINEAR)
@@ -218,6 +220,7 @@ MOS_STATUS BltState::SetupBltCopyParam(
     BLT_CHK_STATUS_RETURN(m_osInterface->pfnGetResourceInfo(m_osInterface, outputSurface, &ResDetails));
 
     uint32_t outputHeight = ResDetails.dwHeight;
+    uint32_t outputWidth  = ResDetails.dwWidth;
     uint32_t outputPitch  = ResDetails.dwPitch;
 
     if (outputSurface->TileType != MOS_TILE_LINEAR)
@@ -232,12 +235,15 @@ MOS_STATUS BltState::SetupBltCopyParam(
     pMhwBltParams->dwDstLeft   = ResDetails.RenderOffset.YUV.Y.XOffset;
 
     int planeNum = GetPlaneNum(ResDetails.Format);
-    pMhwBltParams->dwDstRight = ResDetails.dwWidth;
+    pMhwBltParams->dwDstRight = std::min(inputWidth, outputWidth);
 
-    BLT_CHK_NULL_RETURN(outputSurface->pGmmResInfo);
     if (outputSurface->pGmmResInfo->GetResourceType() != RESOURCE_BUFFER)
     {
         BytesPerTexel = outputSurface->pGmmResInfo->GetBitsPerPixel() / 8;  // using Bytes.
+        if (ResDetails.Format == Format_P010 || ResDetails.Format == Format_P016)
+        {
+            BytesPerTexel = 2;
+        }
     }
 
     if (true == m_blokCopyon)
@@ -260,9 +266,8 @@ MOS_STATUS BltState::SetupBltCopyParam(
     }
     else
     {
-        BLT_CHK_NULL_RETURN(outputSurface->pGmmResInfo);
-        int bytePerTexelScaling = GetBytesPerTexelScaling(ResDetails.Format);
-        pMhwBltParams->dwDstBottom = ResDetails.dwHeight;
+        int bytePerTexelScaling    = GetBytesPerTexelScaling(ResDetails.Format);
+        pMhwBltParams->dwDstBottom = std::min(inputHeight, outputHeight);
 
         if (1 == planeIndex || 2 == planeIndex)
         {
@@ -274,7 +279,6 @@ MOS_STATUS BltState::SetupBltCopyParam(
                pMhwBltParams->dwDstRight  = pMhwBltParams->dwDstRight / 2;
                pMhwBltParams->dwDstBottom = pMhwBltParams->dwDstBottom / 2;
            }
-
         }
     }
     pMhwBltParams->pSrcOsResource = inputSurface;
@@ -494,7 +498,9 @@ uint32_t BltState::GetFastCopyColorDepth(
 
    switch (format)
    {
-       case Format_NV12:
+        case Format_NV12:
+        case Format_P010:
+        case Format_P016:
             dstBytesPerTexel = 2;
            break;
 
