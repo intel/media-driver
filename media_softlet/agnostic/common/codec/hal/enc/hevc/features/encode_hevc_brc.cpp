420be6fe04be3bc683e8e7314cb21ae76648db7b
TianhaoLiu <tianhao.liu@intel.com>
2022-07-15 14:20:10 +0800

    [Encode] Exclude vbvbuffer error handling for ICQ
    Not check the size of vbvbuffer to resolve ICQ Linux func

--- media_softlet/agnostic/common/codec/hal/enc/hevc/features/encode_hevc_brc.cpp
+++ media_softlet/agnostic/common/codec/hal/enc/hevc/features/encode_hevc_brc.cpp
@@ -507,7 +507,7 @@ namespace encode
             int8_t DevThreshI0_S8[8] = {};
 
             uint64_t inputbitsperframe = uint64_t(hucVdencBrcInitDmem->MaxRate_U32*100. / (hucVdencBrcInitDmem->FrameRateM_U32 * 100.0 / hucVdencBrcInitDmem->FrameRateD_U32));
-            if (m_brcEnabled && !hucVdencBrcInitDmem->BufSize_U32)
+            if (m_brcEnabled && (m_rcMode != RATECONTROL_ICQ) && !hucVdencBrcInitDmem->BufSize_U32)
             {
                 ENCODE_ASSERTMESSAGE("VBV BufSize should not be 0 for BRC case\n");
                 return MOS_STATUS_INVALID_PARAMETER;
