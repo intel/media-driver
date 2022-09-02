91ca9a1ca6066396e4a05af102a55b977c7d5c65
Haihao Xiang <haihao.xiang@intel.com>
2022-09-02 12:55:27 +0800

    media_libva_vp: add support for XYUV
    XYUV is used in 4:4:4 8bit content decoding and encoding, so we need to
    support this format in vp too

--- media_driver/linux/common/ddi/media_libva_caps.h
+++ media_driver/linux/common/ddi/media_libva_caps.h
@@ -844,7 +844,7 @@ protected:
 #endif
     static const uint16_t m_maxProfiles = 17; //!< Maximum number of supported profiles
     static const uint16_t m_maxProfileEntries = 64; //!< Maximum number of supported profile & entrypoint combinations
-    static const uint32_t m_numVpSurfaceAttr = 20; //!< Number of VP surface attributes
+    static const uint32_t m_numVpSurfaceAttr = 21; //!< Number of VP surface attributes
     static const uint32_t m_numJpegSurfaceAttr = 8; //!< Number of JPEG surface attributes
     static const uint32_t m_numJpegEncSurfaceAttr = 5; //!< Number of JPEG encode surface attributes
     static const uint16_t m_maxEntrypoints = 7; //!<  Maximum number of supported entrypoints
