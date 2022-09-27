ca5c333fa5ebf2ff173ec1707b2d8d980d589bc0
hanlong1 <longfei.han@intel.com>
2022-09-28 00:32:59 +0800

    Revert "[Decode] Fix HEVC SCC MD5 mismatch"
    This reverts commit intel-innersource/drivers.gpu.unified#b4e2f3437

--- media_softlet/agnostic/common/codec/hal/dec/hevc/features/decode_hevc_basic_feature.cpp
+++ media_softlet/agnostic/common/codec/hal/dec/hevc/features/decode_hevc_basic_feature.cpp
@@ -435,13 +435,13 @@ MOS_STATUS HevcBasicFeature::CreateReferenceBeforeLoopFilter()
     if (m_referenceBeforeLoopFilter == nullptr)
     {
         m_referenceBeforeLoopFilter = m_allocator->AllocateSurface(
-            m_destSurface.dwWidth, m_destSurface.dwHeight, "Reference before loop filter",
+            m_destSurface.dwPitch, m_destSurface.dwHeight, "Reference before loop filter",
             m_destSurface.Format, m_destSurface.bCompressible, resourceOutputPicture, notLockableVideoMem);
         DECODE_CHK_NULL(m_referenceBeforeLoopFilter);
     }
     else
     {
-        m_allocator->Resize(m_referenceBeforeLoopFilter, m_destSurface.dwWidth, m_destSurface.dwHeight,
+        m_allocator->Resize(m_referenceBeforeLoopFilter, m_destSurface.dwPitch, m_destSurface.dwHeight,
             notLockableVideoMem, false, "Reference before loop filter");
     }
     return MOS_STATUS_SUCCESS;
