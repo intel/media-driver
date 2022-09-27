9abd8481b014a1f473e8785ec24b9e3b7c437386
XuZhengguo <zhengguo.xu@intel.com>
2022-09-27 08:42:35 +0800

    [Decode] Scalability reg key not work
    Reg key unable to disable decode scalability

--- media_driver/media_interface/media_interfaces_dg2/media_interfaces_dg2.cpp
+++ media_driver/media_interface/media_interfaces_dg2/media_interfaces_dg2.cpp
@@ -572,7 +572,7 @@ MOS_STATUS CodechalInterfacesXe_Hpm::Initialize(
 
     if (CodecHalIsDecode(CodecFunction))
     {
-        if(osInterface->bHcpDecScalabilityMode = MOS_SCALABILITY_ENABLE_MODE_USER_FORCE)
+        if(osInterface->bHcpDecScalabilityMode == MOS_SCALABILITY_ENABLE_MODE_USER_FORCE)
         {
             disableScalability = false;
         }
