8fb7394b95d92b5bcdabceda833899ecccb701bb
Gu, Lihao <lihao.gu@intel.com>
2022-10-24 22:05:40 +0800

    Fix build failure with full free kernel
    Fixed #1521
    Signed-off-by: Gu, Lihao <lihao.gu@intel.com>

--- media_driver/agnostic/Xe_M/Xe_XPM/vp/media_srcs.cmake
+++ media_driver/agnostic/Xe_M/Xe_XPM/vp/media_srcs.cmake
@@ -19,5 +19,7 @@
 # OTHER DEALINGS IN THE SOFTWARE.
 
 media_include_subdirectory(hal)
+if(ENABLE_KERNELS AND ENABLE_NONFREE_KERNELS)
 media_include_subdirectory(kernel)
+endif()
 media_include_subdirectory(kdll)
