50c8b554c7ec32757ea84d920210b5d95dd0a2ca
pengwan1 <peng.x.wang@intel.com>
2022-11-18 10:59:40 +0800

    [VP] Fix TDR issue with 4096 width
    Fix TDR issue with 4096 width, change the round to zero to calculate the vesfc state

--- media_driver/agnostic/Xe_M/Xe_XPM/hw/mhw_sfc_xe_xpm.cpp
+++ media_driver/agnostic/Xe_M/Xe_XPM/hw/mhw_sfc_xe_xpm.cpp
@@ -288,11 +288,12 @@ MOS_STATUS MhwSfcInterfaceXe_Xpm::AddSfcState(
     // Set DW13
     cmd.DW13.AlphaDefaultValue           = MOS_CLAMP_MIN_MAX(MOS_F_ROUND(pSfcStateparamsXe_Xpm->fAlphaPixel * 1024.0F), 0, 1023); // U10
 
+    // Use round to zero for the scaling factor calculation to resolve tdr issue in scalability case
     // Set DW14
-    cmd.DW14.ScalingFactorHeight         = MOS_UF_ROUND((double)pSfcStateparamsXe_Xpm->dwSourceRegionHeight / (double)pSfcStateparamsXe_Xpm->dwScaledRegionHeight * 524288.0F); // U4.19
+    cmd.DW14.ScalingFactorHeight         = (uint32_t)((double)pSfcStateparamsXe_Xpm->dwSourceRegionHeight / (double)pSfcStateparamsXe_Xpm->dwScaledRegionHeight * 524288.0F); // U4.19
 
     // Set DW15
-    cmd.DW15.ScaleFactorWidth          = MOS_UF_ROUND((double)pSfcStateparamsXe_Xpm->dwSourceRegionWidth / (double)pSfcStateparamsXe_Xpm->dwScaledRegionWidth * 524288.0F); // U4.19
+    cmd.DW15.ScaleFactorWidth          = (uint32_t)((double)pSfcStateparamsXe_Xpm->dwSourceRegionWidth / (double)pSfcStateparamsXe_Xpm->dwScaledRegionWidth * 524288.0F); // U4.19
 
     // Set DW19
     if (pSfcStateparamsXe_Xpm->bMMCEnable            &&
