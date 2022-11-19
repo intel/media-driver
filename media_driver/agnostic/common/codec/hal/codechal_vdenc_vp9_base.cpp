750d3f2bf90027d297854cd20125e3076ff19749
IlyaSergeyev <ilya.sergeev@intel.com>
2022-11-20 02:23:49 +0800

    [VP9][Encode] Unaligned height corruption - fix U-plane offset for Linux
    1. Different U plane offset getting for Linux
    2. Ignore bigger real size of the recon surface

--- media_driver/agnostic/common/codec/hal/codechal_vdenc_vp9_base.cpp
+++ media_driver/agnostic/common/codec/hal/codechal_vdenc_vp9_base.cpp
@@ -4338,11 +4338,6 @@ MOS_STATUS CodechalVdencVp9State::SetHcpSrcSurfaceParams(MHW_VDBOX_SURFACE_PARAM
 
     CODECHAL_ENCODE_FUNCTION_ENTER;
 
-    if (MEDIA_IS_WA(m_waTable, Wa_Vp9UnalignedHeight) && (m_rawSurfaceToPak->Format == Format_NV12 || m_rawSurfaceToPak->Format == Format_P010))
-    {
-        m_rawSurfaceToPak->dwHeight = m_oriFrameHeight;
-    }
-
     if (m_pictureCodingType != I_TYPE)
     {
         uint8_t refPicIndex = 0, scalingIdx = 0;
