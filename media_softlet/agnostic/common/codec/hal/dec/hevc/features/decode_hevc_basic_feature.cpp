7f42e29b155461629b2bccb397a9a1052a14fa30
XuanJessica <xuan.xuan@intel.com>
2022-11-30 18:45:27 +0800

    Revert "[Decode] HEVC Long Format Parameters Sanity Check"
    This reverts commit 19dbeddf3.

--- media_softlet/agnostic/common/codec/hal/dec/hevc/features/decode_hevc_basic_feature.cpp
+++ media_softlet/agnostic/common/codec/hal/dec/hevc/features/decode_hevc_basic_feature.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2019-2022, Intel Corporation
+* Copyright (c) 2019, Intel Corporation
 *
 * Permission is hereby granted, free of charge, to any person obtaining a
 * copy of this software and associated documentation files (the "Software"),
@@ -24,7 +24,6 @@
 //! \brief    Defines the common interface for decode hevc parameter
 //!
 
-#include <set>
 #include "decode_hevc_basic_feature.h"
 #include "decode_utils.h"
 #include "decode_allocator.h"
@@ -132,7 +131,8 @@ MOS_STATUS HevcBasicFeature::ErrorDetectAndConceal()
             "num_tile_columns_minus1 or num_tile_rows_minus1 is out of range!");
 
         // valid range is [0, m_widthInCtb - 1]
-        if (m_hevcPicParams->num_tile_columns_minus1 > m_widthInCtb - 1)
+        if (m_hevcPicParams->num_tile_columns_minus1 < 0 ||
+            m_hevcPicParams->num_tile_columns_minus1 > m_widthInCtb - 1)
         {
             DECODE_ASSERTMESSAGE("num_tile_columns_minus1 out of range: %d\n",
                 m_hevcPicParams->num_tile_columns_minus1);
@@ -141,7 +141,8 @@ MOS_STATUS HevcBasicFeature::ErrorDetectAndConceal()
         }
 
         // valid range is [0, m_heightInCtb - 1]
-        if (m_hevcPicParams->num_tile_rows_minus1 > m_heightInCtb - 1)
+        if (m_hevcPicParams->num_tile_rows_minus1 < 0 ||
+            m_hevcPicParams->num_tile_rows_minus1 > m_heightInCtb - 1)
         {
             DECODE_ASSERTMESSAGE("num_tile_rows_minus1 out of range: %d\n",
                 m_hevcPicParams->num_tile_rows_minus1);
@@ -181,7 +182,8 @@ MOS_STATUS HevcBasicFeature::ErrorDetectAndConceal()
     }
 
     // diff_cu_qp_delta_depth range is [0, log2_diff_max_min_luma_coding_block_size]
-    if (m_hevcPicParams->diff_cu_qp_delta_depth > m_hevcPicParams->log2_diff_max_min_luma_coding_block_size)
+    if (m_hevcPicParams->diff_cu_qp_delta_depth < 0 ||
+        m_hevcPicParams->diff_cu_qp_delta_depth > m_hevcPicParams->log2_diff_max_min_luma_coding_block_size)
     {
         DECODE_ASSERTMESSAGE("diff_cu_qp_delta_depth %d is invalid\n", m_hevcPicParams->diff_cu_qp_delta_depth);
         return MOS_STATUS_INVALID_PARAMETER;
@@ -202,7 +204,7 @@ MOS_STATUS HevcBasicFeature::ErrorDetectAndConceal()
     }
 
     // log2_parallel_merge_level_minus2 is in range [0, 4]
-    if (m_hevcPicParams->log2_parallel_merge_level_minus2 > 4)
+    if (m_hevcPicParams->log2_parallel_merge_level_minus2 < 0 || m_hevcPicParams->log2_parallel_merge_level_minus2 > 4)
     {
         DECODE_ASSERTMESSAGE("log2_parallel_merge_level_minus2 is out of range\n");
         return MOS_STATUS_INVALID_PARAMETER;
@@ -234,7 +236,8 @@ MOS_STATUS HevcBasicFeature::ErrorDetectAndConceal()
             DECODE_ASSERTMESSAGE("log2_max_transform_skip_block_size_minus2 should equal to 0 when transform_skip_enabled_flag not present\n");
         }
 
-        if (m_hevcRextPicParams->diff_cu_chroma_qp_offset_depth > m_hevcPicParams->log2_diff_max_min_luma_coding_block_size)
+        if (m_hevcRextPicParams->diff_cu_chroma_qp_offset_depth < 0
+            || m_hevcRextPicParams->diff_cu_chroma_qp_offset_depth > m_hevcPicParams->log2_diff_max_min_luma_coding_block_size)
         {
             DECODE_ASSERTMESSAGE("diff_cu_chroma_qp_offset_depth is out of range\n");
         }
@@ -246,14 +249,16 @@ MOS_STATUS HevcBasicFeature::ErrorDetectAndConceal()
         }
 
         // chroma_qp_offset_list_len_minus1 range is [0, 5]
-        if (m_hevcRextPicParams->chroma_qp_offset_list_len_minus1 > 5)
+        if (m_hevcRextPicParams->chroma_qp_offset_list_len_minus1 < 0
+            || m_hevcRextPicParams->chroma_qp_offset_list_len_minus1 > 5)
         {
             DECODE_ASSERTMESSAGE("chroma_qp_offset_list_len_minus1 is out of range [0, 5]\n");
         }
 
         // check if TU size is valid
         auto maxTUSize = m_hevcPicParams->log2_diff_max_min_transform_block_size + m_hevcPicParams->log2_min_transform_block_size_minus2;
-        if (m_hevcRextPicParams->log2_max_transform_skip_block_size_minus2 > maxTUSize)
+        if (m_hevcRextPicParams->log2_max_transform_skip_block_size_minus2 < 0 ||
+            m_hevcRextPicParams->log2_max_transform_skip_block_size_minus2 > maxTUSize)
         {
             DECODE_ASSERTMESSAGE("log2_max_transform_skip_block_size_minus2 is out of range\n");
         }
@@ -271,216 +276,35 @@ MOS_STATUS HevcBasicFeature::ErrorDetectAndConceal()
         }
     }
 
-    return MOS_STATUS_SUCCESS;
-}
-
-MOS_STATUS HevcBasicFeature::SliceDataSizeCheck(uint32_t sliceIdx)
-{
-    DECODE_FUNC_CALL();
-
-    if (m_hevcSliceParams[sliceIdx].slice_data_offset + m_hevcSliceParams[sliceIdx].slice_data_size > m_dataSize)
+    if (!m_shortFormatInUse)
     {
-        DECODE_ASSERTMESSAGE("invalid slice %d, data size overflow\n", sliceIdx);
-        return MOS_STATUS_INVALID_PARAMETER;
-    }
-
-    /* Check slice missing data size should not be dropped to 0 */
-    if (m_hevcSliceParams[sliceIdx].slice_data_size <= m_hevcSliceParams[sliceIdx].ByteOffsetToSliceData)
-    {
-        DECODE_ASSERTMESSAGE("slice data size %u should be greater than slice header length %u\n", m_hevcSliceParams[sliceIdx].slice_data_size, m_hevcSliceParams[sliceIdx].ByteOffsetToSliceData);
-        return MOS_STATUS_INVALID_PARAMETER;
-    }
-
-    return MOS_STATUS_SUCCESS;
-}
-
-MOS_STATUS HevcBasicFeature::SliceSegmentAddressCheck(uint32_t sliceIdx, std::vector<int> &sliceSegmentAddressVector)
-{
-    DECODE_FUNC_CALL();
-
-    /* When sliceIdx = 0, it should be an independent slice, and its slice_segment_address should be 0 */
-    if (sliceIdx == 0)
-    {
-        if (!IsIndependentSlice(sliceIdx) || m_hevcSliceParams[sliceIdx].slice_segment_address != 0)
+        if (m_hevcPicParams->tiles_enabled_flag || m_hevcPicParams->entropy_coding_sync_enabled_flag)
         {
-            DECODE_ASSERTMESSAGE("Invalid slice %d, independent slice = %s, slice_segment_address = %d\n",
-                                 sliceIdx, IsIndependentSlice(sliceIdx) ? "True" : "False", m_hevcSliceParams[sliceIdx].slice_segment_address);
-            return MOS_STATUS_INVALID_PARAMETER;
-        }
-    }
-
-    /* Check slice_segment_address should be in range [0, picSizeInCtbsY - 1] and
-     * shall not be equal to the value of slice_segment_address of any other coded slice segment NAL unit of the same coded picture */
-    uint32_t picSizeInCtbsY = m_widthInCtb * m_heightInCtb;
-    if (sliceIdx != 0) // Previous check has conformed when sliceIdx = 0, slice_segment_address = 0
-    {
-        if ((m_hevcSliceParams[sliceIdx].slice_segment_address >= picSizeInCtbsY))
-        {
-            DECODE_ASSERTMESSAGE("slice segment address %d is out of range [0, %d]\n",
-                                 m_hevcSliceParams[sliceIdx].slice_segment_address, picSizeInCtbsY - 1);
-            return MOS_STATUS_INVALID_PARAMETER;
-        }
-    }
-    sliceSegmentAddressVector.push_back(m_hevcSliceParams[sliceIdx].slice_segment_address);
-    if (std::set<int>(sliceSegmentAddressVector.begin(), sliceSegmentAddressVector.end()).size() != sliceSegmentAddressVector.size()) // Has duplicate slice_segment_address
-    {
-        DECODE_ASSERTMESSAGE("%dth slice_segment_address %d has duplicated.\n", sliceIdx, m_hevcSliceParams[sliceIdx].slice_segment_address);
-        return MOS_STATUS_INVALID_PARAMETER;
-    }
-
-    return MOS_STATUS_SUCCESS;
-}
-
-MOS_STATUS HevcBasicFeature::NumEntryPointOffsetsCheck(uint32_t sliceIdx)
-{
-    DECODE_FUNC_CALL();
-
-    /* Check num_entry_point_offsets */
-    if (m_hevcPicParams->tiles_enabled_flag || m_hevcPicParams->entropy_coding_sync_enabled_flag)
-    {
-        if (m_hevcPicParams->tiles_enabled_flag == 0 && m_hevcPicParams->entropy_coding_sync_enabled_flag == 1)
-        {
-            if (m_hevcSliceParams[sliceIdx].num_entry_point_offsets > m_hevcPicParams->PicHeightInMinCbsY - 1)
+            if (m_hevcPicParams->tiles_enabled_flag == 0 && m_hevcPicParams->entropy_coding_sync_enabled_flag == 1)
             {
-                DECODE_ASSERTMESSAGE("num_entry_point_offsets %d is out of range [0, %d]\n",
-                                     m_hevcSliceParams[sliceIdx].num_entry_point_offsets, m_hevcPicParams->PicHeightInMinCbsY - 1);
-                return MOS_STATUS_INVALID_PARAMETER;
+                if (m_hevcSliceParams->num_entry_point_offsets < 0 || m_hevcSliceParams->num_entry_point_offsets > m_hevcPicParams->PicHeightInMinCbsY - 1)
+                {
+                    DECODE_ASSERTMESSAGE("num_entry_point_offsets is out of range\n");
+                    return MOS_STATUS_INVALID_PARAMETER;
+                }
+            }
+            else if (m_hevcPicParams->tiles_enabled_flag == 1 && m_hevcPicParams->entropy_coding_sync_enabled_flag == 0)
+            {
+                if (m_hevcSliceParams->num_entry_point_offsets < 0 || m_hevcSliceParams->num_entry_point_offsets > ((m_hevcPicParams->num_tile_columns_minus1 + 1) * (m_hevcPicParams->num_tile_rows_minus1 + 1) - 1))
+                {
+                    DECODE_ASSERTMESSAGE("num_entry_point_offsets is out of range\n");
+                    return MOS_STATUS_INVALID_PARAMETER;
+                }
+            }
+            else
+            {
+                if (m_hevcSliceParams->num_entry_point_offsets < 0 || m_hevcSliceParams->num_entry_point_offsets > ((m_hevcPicParams->num_tile_columns_minus1 + 1) * m_hevcPicParams->PicHeightInMinCbsY - 1))
+                {
+                    DECODE_ASSERTMESSAGE("num_entry_point_offsets is out of range\n");
+                    return MOS_STATUS_INVALID_PARAMETER;
+                }
             }
         }
-        else if (m_hevcPicParams->tiles_enabled_flag == 1 && m_hevcPicParams->entropy_coding_sync_enabled_flag == 0)
-        {
-            if (m_hevcSliceParams[sliceIdx].num_entry_point_offsets > ((m_hevcPicParams->num_tile_columns_minus1 + 1) * (m_hevcPicParams->num_tile_rows_minus1 + 1) - 1))
-            {
-                DECODE_ASSERTMESSAGE("num_entry_point_offsets %d is out of range [0, %d]\n",
-                                     m_hevcSliceParams[sliceIdx].num_entry_point_offsets, (m_hevcPicParams->num_tile_columns_minus1 + 1) * (m_hevcPicParams->num_tile_rows_minus1 + 1) - 1);
-                return MOS_STATUS_INVALID_PARAMETER;
-            }
-        }
-        else
-        {
-            if (m_hevcSliceParams[sliceIdx].num_entry_point_offsets > ((m_hevcPicParams->num_tile_columns_minus1 + 1) * m_hevcPicParams->PicHeightInMinCbsY - 1))
-            {
-                DECODE_ASSERTMESSAGE("num_entry_point_offsets %d is out of range [0, %d]\n",
-                                     m_hevcSliceParams[sliceIdx].num_entry_point_offsets, (m_hevcPicParams->num_tile_columns_minus1 + 1) * m_hevcPicParams->PicHeightInMinCbsY - 1);
-                return MOS_STATUS_INVALID_PARAMETER;
-            }
-        }
-    }
-    return MOS_STATUS_SUCCESS;
-}
-
-MOS_STATUS HevcBasicFeature::ReferenceParamCheck(uint32_t sliceIdx)
-{
-    DECODE_FUNC_CALL();
-
-    /* If it's P or B slice, the num_ref_idx_lx_active_minus1 should be in range [0, 14] */
-    if (decodeHevcISlice != m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.slice_type)
-    {
-        if (m_hevcSliceParams[sliceIdx].num_ref_idx_l0_active_minus1 >= CODEC_MAX_NUM_REF_FRAME_HEVC )
-        {
-            DECODE_ASSERTMESSAGE("num_ref_idx_l0_active_minus1 %d is out of range [0, 14]\n", m_hevcSliceParams[sliceIdx].num_ref_idx_l0_active_minus1);
-            return MOS_STATUS_INVALID_PARAMETER;
-        }
-        if (decodeHevcBSlice == m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.slice_type)
-        {
-            if (m_hevcSliceParams[sliceIdx].num_ref_idx_l1_active_minus1 >= CODEC_MAX_NUM_REF_FRAME_HEVC)
-            {
-                DECODE_ASSERTMESSAGE("num_ref_idx_l1_active_minus1 %d is out of range [0, 14]\n", m_hevcSliceParams[sliceIdx].num_ref_idx_l1_active_minus1);
-                return MOS_STATUS_INVALID_PARAMETER;
-            }
-        }
-    }
-
-    /* Check RefPicList[listIdx][refIdx].FrameIdx */
-    uint32_t num_ref_idx_active_minus1[2] = {0};
-    num_ref_idx_active_minus1[0] = m_hevcSliceParams[sliceIdx].num_ref_idx_l0_active_minus1;
-    num_ref_idx_active_minus1[1] = m_hevcSliceParams[sliceIdx].num_ref_idx_l1_active_minus1;
-    bool hasRefPic[2] = {false};
-    uint8_t reflistNum = 0;
-
-    switch (m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.slice_type)
-    {
-    case decodeHevcISlice:
-        reflistNum = 0;
-        break;
-    case decodeHevcPSlice:
-        reflistNum = 1;
-        break;
-    case decodeHevcBSlice:
-        reflistNum = 2;
-        break;
-    default:
-        break;
-    }
-
-    for (uint8_t listIdx = 0; listIdx < reflistNum; listIdx++)
-    {
-        /* If num_ref_idx_lx_active_minus1 is in [0, 14], it should have at least 1 reference picture. */
-        for (uint8_t refListIdx = 0; refListIdx <= num_ref_idx_active_minus1[listIdx]; refListIdx++)
-        {
-            if (m_hevcSliceParams[sliceIdx].RefPicList[listIdx][refListIdx].FrameIdx < CODEC_MAX_NUM_REF_FRAME_HEVC)
-            {
-                hasRefPic[listIdx] = true;
-                break;
-            }
-        }
-
-        if (false == hasRefPic[listIdx])
-        {
-            DECODE_ASSERTMESSAGE("num_ref_idx_active_minus1[%d] = %d, RefPicList[%d].FrameIdx is all 127\n",
-                                 listIdx, num_ref_idx_active_minus1[listIdx], listIdx);
-            return MOS_STATUS_INVALID_PARAMETER;
-        }
-    }
-    return MOS_STATUS_SUCCESS;
-}
-
-MOS_STATUS HevcBasicFeature::CollocatedRefIdxCheck(uint32_t sliceIdx)
-{
-    DECODE_FUNC_CALL();
-
-    if (m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.slice_temporal_mvp_enabled_flag)
-    {
-        /* Check collocated_ref_idx */
-        if (((m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.slice_type == decodeHevcPSlice) || (m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.slice_type == decodeHevcBSlice))
-            && m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.collocated_from_l0_flag)
-        {
-            if (m_hevcSliceParams[sliceIdx].collocated_ref_idx > m_hevcSliceParams[sliceIdx].num_ref_idx_l0_active_minus1)
-            {
-                DECODE_ASSERTMESSAGE("collocated_ref_idx %d is out of range [%d, %d]\n", m_hevcSliceParams[sliceIdx].collocated_ref_idx, 0, m_hevcSliceParams[sliceIdx].num_ref_idx_l0_active_minus1);
-                return MOS_STATUS_INVALID_PARAMETER;
-            }
-        }
-        if ((m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.slice_type == decodeHevcBSlice) && (m_hevcSliceParams[sliceIdx].LongSliceFlags.fields.collocated_from_l0_flag == 0))
-        {
-            if (m_hevcSliceParams[sliceIdx].collocated_ref_idx > m_hevcSliceParams[sliceIdx].num_ref_idx_l1_active_minus1)
-            {
-                DECODE_ASSERTMESSAGE("collocated_ref_idx %d is out of range [%d, %d]\n", m_hevcSliceParams[sliceIdx].collocated_ref_idx, 0, m_hevcSliceParams[sliceIdx].num_ref_idx_l1_active_minus1);
-                return MOS_STATUS_INVALID_PARAMETER;
-            }
-        }
-    }
-
-    return MOS_STATUS_SUCCESS;
-}
-
-MOS_STATUS HevcBasicFeature::ErrorDetectAndConcealForLongFormat()
-{
-    DECODE_FUNC_CALL();
-
-    std::vector<int> sliceSegmentAddressVector;
-
-    for (uint32_t sliceIdx = 0; sliceIdx < m_numSlices; sliceIdx++)
-    {
-        DECODE_CHK_STATUS(SliceDataSizeCheck(sliceIdx));
-
-        DECODE_CHK_STATUS(SliceSegmentAddressCheck(sliceIdx, sliceSegmentAddressVector));
-
-        DECODE_CHK_STATUS(NumEntryPointOffsetsCheck(sliceIdx));
-
-        DECODE_CHK_STATUS(ReferenceParamCheck(sliceIdx));
-
-        DECODE_CHK_STATUS(CollocatedRefIdxCheck(sliceIdx));
     }
 
     return MOS_STATUS_SUCCESS;
@@ -597,11 +421,6 @@ MOS_STATUS HevcBasicFeature::SetSliceStructs()
     {
         PCODEC_HEVC_SLICE_PARAMS lastSlice = m_hevcSliceParams + (m_numSlices - 1);
         DECODE_CHK_STATUS(SetRequiredBitstreamSize(lastSlice->slice_data_offset + lastSlice->slice_data_size));
-    }
-
-    if (!m_shortFormatInUse)
-    {
-        DECODE_CHK_STATUS(ErrorDetectAndConcealForLongFormat());
     }
 
     return MOS_STATUS_SUCCESS;
