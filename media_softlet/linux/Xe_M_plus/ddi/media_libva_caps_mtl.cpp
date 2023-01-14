80d5a06425b4750a085bada7279f70aa56264580
Hu_Molly <molly.hu@intel.com>
2023-01-14 11:32:10 +0800

    [Media Common] [VP] add media copy caps
    add media copy caps requirement

--- media_softlet/linux/Xe_M_plus/ddi/media_libva_caps_mtl.cpp
+++ media_softlet/linux/Xe_M_plus/ddi/media_libva_caps_mtl.cpp
@@ -29,6 +29,44 @@
 #include "media_libva_util.h"
 #include "media_libva_caps_mtl.h"
 #include "media_libva_caps_factory.h"
+#define VA_COPY_MODE_BALANCE 3
+
+VAStatus MediaLibvaCapsMtl::GetDisplayAttributes(
+    VADisplayAttribute* attribList,
+    int32_t numAttribs)
+{
+    DDI_CHK_NULL(attribList, "Null attribList", VA_STATUS_ERROR_INVALID_PARAMETER);
+    for (auto i = 0; i < numAttribs; i++)
+    {
+        switch (attribList->type)
+        {
+        #if VA_CHECK_VERSION(1, 15, 0)
+        case VADisplayPCIID:
+            attribList->min_value = attribList->value = attribList->max_value = (m_mediaCtx->iDeviceId & 0xffff) | 0x80860000;
+            attribList->flags = VA_DISPLAY_ATTRIB_GETTABLE;
+            break;
+        #endif
+        case VADisplayAttribCopy:
+            attribList->min_value = attribList->value = attribList->max_value =
+                (1 << VA_EXEC_MODE_POWER_SAVING) | (1 << VA_EXEC_MODE_PERFORMANCE) | (1 << VA_EXEC_MODE_DEFAULT) | (1 << VA_COPY_MODE_BALANCE);
+            // 1000:balance mode: Vebox Copy
+            // 100: perfromance : Render copy
+            // 10:  POWER_SAVING: BLT copy
+            // 1:   default model
+            // 0:   don't support media copy.
+            attribList->flags = VA_DISPLAY_ATTRIB_GETTABLE;
+            break;
+        default:
+            attribList->min_value = VA_ATTRIB_NOT_SUPPORTED;
+            attribList->max_value = VA_ATTRIB_NOT_SUPPORTED;
+            attribList->value = VA_ATTRIB_NOT_SUPPORTED;
+            attribList->flags = VA_DISPLAY_ATTRIB_NOT_SUPPORTED;
+            break;
+        }
+        attribList++;
+    }
+    return VA_STATUS_SUCCESS;
+}
 
 static bool mtlRegistered = MediaLibvaCapsFactory<MediaLibvaCaps, DDI_MEDIA_CONTEXT>::
     RegisterCaps<MediaLibvaCapsMtl>((uint32_t)IGFX_METEORLAKE);
