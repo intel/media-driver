b5175cd73a3ccc9043699d72d73a93c783462ffe
Dylan-debug <jianxing.cai@intel.com>
2023-04-07 23:59:01 +0800

    [Media Common] [VP] fix dead lock
    fix dead lock!

--- media_softlet/agnostic/common/os/mos_cmdbufmgr_next.cpp
+++ media_softlet/agnostic/common/os/mos_cmdbufmgr_next.cpp
@@ -101,22 +101,27 @@ MOS_STATUS CmdBufMgrNext::Reset()
     CommandBufferNext *cmdBuf = nullptr;
     auto gpuContextMgr      = m_osContext->GetGpuContextMgr();
     MOS_OS_CHK_NULL_RETURN(gpuContextMgr);
-
-    MosUtilities::MosLockMutex(m_availablePoolMutex);
+    std::vector<CommandBufferNext *> tmpInUseCmdBufPool = {};
 
     MosUtilities::MosLockMutex(m_inUsePoolMutex);
 
     if (!m_inUseCmdBufPool.empty())
     {
-        for (auto& cmdBuf : m_inUseCmdBufPool)
-        {
-            UpperInsert(cmdBuf);
-        }
+        tmpInUseCmdBufPool = std::move(m_inUseCmdBufPool);
     }
 
     // clear in-use command buffer pool
     m_inUseCmdBufPool.clear();
     MosUtilities::MosUnlockMutex(m_inUsePoolMutex);
+
+    MosUtilities::MosLockMutex(m_availablePoolMutex);
+    if (!tmpInUseCmdBufPool.empty())
+    {
+        for (auto& cmdBuf : tmpInUseCmdBufPool)
+        {
+            UpperInsert(cmdBuf);
+        }
+    }
 
     for (auto& cmdBuf : m_availableCmdBufPool)
     {
