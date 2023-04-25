588ac0aff86b455c9fd088c1c1176fe613541986
dmitrybrazhkin <dmitry.brazhkin@intel.com>
2023-04-25 19:53:58 +0800

    [Encode] Fix memory allocation for AVC VDEnc Encode statistic
    Move statistics buffers allocation from driver to RT

--- media_driver/agnostic/common/codec/hal/codechal_encoder_base.cpp
+++ media_driver/agnostic/common/codec/hal/codechal_encoder_base.cpp
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2017-2021, Intel Corporation
+* Copyright (c) 2017-2023, Intel Corporation
 *
 * Permission is hereby granted, free of charge, to any person obtaining a
 * copy of this software and associated documentation files (the "Software"),
@@ -4119,6 +4119,9 @@ MOS_STATUS CodechalEncoderState::GetStatusReport(
         PCODEC_REF_LIST refList = encodeStatusReport->pCurrRefList;
         uint32_t localCount = encodeStatus->dwStoredData - globalHWStoredData;
 
+        encodeStatusReport->pFrmStatsInfo = codecStatus[i].pFrmStatsInfo;
+        encodeStatusReport->pBlkStatsInfo = codecStatus[i].pBlkStatsInfo;
+
         if (localCount == 0 || localCount > globalCount)
         {
             CODECHAL_DEBUG_TOOL(
@@ -4260,7 +4263,7 @@ MOS_STATUS CodechalEncoderState::GetStatusReport(
                     return eStatus;
                 }
 
-                CODECHAL_ENCODE_CHK_STATUS_RETURN(GetStatusReportExt(encodeStatus, encodeStatusReport, i == (numStatus - 1)));
+                CODECHAL_ENCODE_CHK_STATUS_RETURN(GetStatusReportExt(encodeStatus, encodeStatusReport, index));
 
                 if (m_osInterface->osCpInterface->IsCpEnabled() && m_skipFrameBasedHWCounterRead == false)
                 {
