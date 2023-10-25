d968eb254580fced37bbb88ae2fffd42a4408178
Chen_Hao5 <hao5.chen@intel.com>
2023-10-25 14:35:28 +0800

    [Decode] [DVT] Refresh Device IDs for ADL-S to keep align with Specification
    Refresh Device IDs for ADL-S to keep align with Specification. Add some new device IDs.

--- media_driver/linux/gen12/ddi/media_sysinfo_g12.cpp
+++ media_driver/linux/gen12/ddi/media_sysinfo_g12.cpp
@@ -383,6 +383,12 @@ static bool adlsGt1Device4683 = DeviceInfoFactory<GfxDeviceInfo>::
 static bool adlsGt1Device4688 = DeviceInfoFactory<GfxDeviceInfo>::
     RegisterDevice(0x4688, &adlsGt1Info);
 
+static bool adlsGt1Device468A = DeviceInfoFactory<GfxDeviceInfo>::
+    RegisterDevice(0x468A, &adlsGt1Info);
+
+static bool adlsGt1Device468B = DeviceInfoFactory<GfxDeviceInfo>::
+    RegisterDevice(0x468B, &adlsGt1Info);
+
 static bool adlsGt1Device4690 = DeviceInfoFactory<GfxDeviceInfo>::
     RegisterDevice(0x4690, &adlsGt1Info);
 
