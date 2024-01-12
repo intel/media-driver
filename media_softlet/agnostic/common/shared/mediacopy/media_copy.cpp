f4a90da123d521b599a09630fcf2fdd401c7957e
JayYang <jay.yang@intel.com>
2024-01-12 10:49:36 +0800

    [Media Common] [VP] Skip checking resource for CP buffer copy
    CP will handle the overflown buffer size itself. Skip normal resource validation check.

--- media_softlet/agnostic/common/shared/mediacopy/media_copy.cpp
+++ media_softlet/agnostic/common/shared/mediacopy/media_copy.cpp
@@ -323,6 +323,22 @@ MOS_STATUS CheckResourceSizeValidForCopy(MOS_SURFACE &res, MCPY_ENGINE method)
     return MOS_STATUS_SUCCESS;
 }
 
+MOS_STATUS ValidateResource(MOS_SURFACE &src, MOS_SURFACE &dst, MCPY_ENGINE method)
+{
+    // For CP buffer copy, CP will handle the overflown size, skip size check
+    if (src.OsResource.pGmmResInfo->GetResourceType() == RESOURCE_BUFFER &&
+        dst.OsResource.pGmmResInfo->GetResourceType() == RESOURCE_BUFFER &&
+        method == MCPY_ENGINE_BLT)
+    {
+        return MOS_STATUS_SUCCESS;
+    }
+
+    MCPY_CHK_STATUS_RETURN(CheckResourceSizeValidForCopy(src, method));
+    MCPY_CHK_STATUS_RETURN(CheckResourceSizeValidForCopy(dst, method));
+
+    return MOS_STATUS_SUCCESS;
+}
+
 //!
 //! \brief    surface copy func.
 //! \details  copy surface.
@@ -340,8 +356,10 @@ MOS_STATUS MediaCopyBaseState::SurfaceCopy(PMOS_RESOURCE src, PMOS_RESOURCE dst,
     MOS_SURFACE SrcResDetails, DstResDetails;
     MOS_ZeroMemory(&SrcResDetails, sizeof(MOS_SURFACE));
     MOS_ZeroMemory(&DstResDetails, sizeof(MOS_SURFACE));
-    SrcResDetails.Format = Format_Invalid;
-    DstResDetails.Format = Format_Invalid;
+    SrcResDetails.Format     = Format_Invalid;
+    SrcResDetails.OsResource = *src;
+    DstResDetails.Format     = Format_Invalid;
+    DstResDetails.OsResource = *dst;
 
     MCPY_STATE_PARAMS     mcpySrc = {nullptr, MOS_MMC_DISABLED, MOS_TILE_LINEAR, MCPY_CPMODE_CLEAR, false};
     MCPY_STATE_PARAMS     mcpyDst = {nullptr, MOS_MMC_DISABLED, MOS_TILE_LINEAR, MCPY_CPMODE_CLEAR, false};
@@ -396,8 +414,7 @@ MOS_STATUS MediaCopyBaseState::SurfaceCopy(PMOS_RESOURCE src, PMOS_RESOURCE dst,
 
     CopyEnigneSelect(preferMethod, mcpyEngine, mcpyEngineCaps);
 
-    MCPY_CHK_STATUS_RETURN(CheckResourceSizeValidForCopy(SrcResDetails, mcpyEngine));
-    MCPY_CHK_STATUS_RETURN(CheckResourceSizeValidForCopy(DstResDetails, mcpyEngine));
+    MCPY_CHK_STATUS_RETURN(ValidateResource(SrcResDetails, DstResDetails, mcpyEngine));
 
     MCPY_CHK_STATUS_RETURN(TaskDispatch(mcpySrc, mcpyDst, mcpyEngine));
 
