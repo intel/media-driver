f6367257a997ac328fd8761f39fbf88c06788057
Bai, Isaac <isaac.bai@intel.com>
2024-07-25 09:54:56 +0800

    [HEVCe] fix a build issue
    fix PCODECHAL_ENC_HEVC_B_MB_ENC_CURBE_G9 build issue

--- media_driver/agnostic/gen9/codec/hal/codechal_encode_hevc_g9.cpp
+++ media_driver/agnostic/gen9/codec/hal/codechal_encode_hevc_g9.cpp
@@ -6182,7 +6182,11 @@ MOS_STATUS CodechalEncHevcStateG9::SetupBrcConstantTable(PMOS_SURFACE brcConstan
     else
     {
         uint32_t curbeSize = 0;
-        PCODECHAL_ENC_HEVC_B_MB_ENC_CURBE_G9 curbe = (PCODECHAL_ENC_HEVC_B_MB_ENC_CURBE_G9)GetDefaultCurbeEncBKernel(curbeSize);
+        void* defaultCurbe = (void*)GetDefaultCurbeEncBKernel(curbeSize);
+        CODECHAL_ENCODE_ASSERT(defaultCurbe);
+
+        CODECHAL_ENC_HEVC_B_MB_ENC_CURBE_G9 cmd, *curbe = &cmd;
+        CODECHAL_ENCODE_CHK_STATUS_RETURN(MOS_SecureMemcpy(curbe, sizeof(cmd), defaultCurbe, curbeSize));
 
         if(curbe == nullptr)
         {
