9306d43ea92d83d8a1318335a3a34bb8c6567f17
bai-isaac <isaac.bai@intel.com>
2024-09-06 14:49:48 +0800

    [Encode] update avc img state program
    update avc img state program.

--- media_softlet/agnostic/Xe_M_plus/Xe2_HPM/hw/vdbox/mhw_vdbox_vdenc_hwcmd_xe2_hpm.h
+++ media_softlet/agnostic/Xe_M_plus/Xe2_HPM/hw/vdbox/mhw_vdbox_vdenc_hwcmd_xe2_hpm.h
@@ -1707,8 +1707,6 @@ struct _VDENC_AVC_IMG_STATE_CMD
     {
         MOS_ZeroMemory(this, sizeof(*this));
         DW0.Value                                        = 0x70850018;
-        DW1.Value                                        = 0x18180000;
-        DW2.Value                                        = 0x00800000;
         DW22.Value                                       = 0x33000000;
     }
 
