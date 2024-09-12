c8c3b96789fac80cfd058d5fbd63dd2740e086fe
Wei_Yuqin <yuqin.wei@intel.com>
2024-09-12 11:42:34 +0800

    [Media Common][VP] APO Mos: Create/free resource with GraphicsResourceNext
    1. In OsContextSpecificNext::Init, create memory manager
    2. create/free/lock/ etc mos interface switch
    3. mos copy interface switch

--- media_softlet/agnostic/common/os/mos_graphicsresource_next.h
+++ media_softlet/agnostic/common/os/mos_graphicsresource_next.h
@@ -499,6 +499,7 @@ protected:
     //! \brief   mos resource usage type, set mocs index
     //!
     MOS_HW_RESOURCE_DEF m_mocsMosResUsageType = MOS_MP_RESOURCE_USAGE_DEFAULT;
+    OsContextNext       *m_osContextPtr = nullptr;
 MEDIA_CLASS_DEFINE_END(GraphicsResourceNext)
 };
 #endif // #ifndef __MOS_GRAPHICS_RESOURCE_NEXT_H__
