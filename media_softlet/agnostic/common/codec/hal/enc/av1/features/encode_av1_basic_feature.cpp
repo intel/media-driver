b75d53669810d28bb298eade43954a749924e37e
Christa03 <caihong.ma@intel.com>
2024-11-18 12:04:37 +0800

    [Encode] Fix AV1e linux A2RGB10 issue
    * [Encode] Fix AV1e linux A2RGB10 issue
    
    Change B10G10A10A2 format depth as 10

--- media_softlet/agnostic/common/codec/hal/enc/av1/features/encode_av1_basic_feature.cpp
+++ media_softlet/agnostic/common/codec/hal/enc/av1/features/encode_av1_basic_feature.cpp
@@ -351,6 +351,8 @@ MOS_STATUS Av1BasicFeature::UpdateFormat(void *params)
     switch(m_rawSurface.Format)
     {
     case Format_P010:
+    case Format_R10G10B10A2:
+    case Format_B10G10R10A2:
         m_is10Bit  = true;
         m_bitDepth = 10;
         break;
