142622d7854d697be5cea56b7927ea9da4451aaa
TonyYe <tony.ye@intel.com>
2025-01-29 11:00:11 +0800

    Fix forced VDBox debug override issue for decode and encode
    1) allow debug override for sigle submission too
    2) get the VDBoxInfo from MediaEngineInfo instead of gt SystemInfo
    3) allow debug override for encode
    4) initialize m_scalabilitySupported

--- media_softlet/agnostic/common/shared/mediacontext/media_context.cpp
+++ media_softlet/agnostic/common/shared/mediacontext/media_context.cpp
@@ -62,6 +62,13 @@ MediaContext::MediaContext(uint8_t componentType, void *hwInterface, PMOS_INTERF
 
     m_streamId = m_osInterface->streamIndex;
     m_gpuContextAttributeTable.clear();
+
+    if (m_numVdbox > 1
+        && m_osInterface != nullptr
+        && m_osInterface->bHcpDecScalabilityMode)
+    {
+        m_scalabilitySupported = true;
+    }
 }
 
 MediaContext::~MediaContext()
@@ -488,6 +495,7 @@ MOS_STATUS MediaContext::CheckScalabilityOverrideValidity()
     MOS_STATUS eStatus = MOS_STATUS_SUCCESS;
 
     MEDIA_SYSTEM_INFO *gtSystemInfo = nullptr;
+    MEDIA_ENGINE_INFO  mediaEngineInfo = {};
     uint32_t           forceVdbox   = 0;
     bool               scalableDecMode = false;
     bool               useVD1          = false;
@@ -496,8 +504,17 @@ MOS_STATUS MediaContext::CheckScalabilityOverrideValidity()
     MHW_MI_CHK_NULL(m_osInterface);
     scalableDecMode = m_osInterface->bHcpDecScalabilityMode ? true : false;
     forceVdbox      = m_osInterface->eForceVdbox;
-    gtSystemInfo    = m_osInterface->pfnGetGtSystemInfo(m_osInterface);
-    MHW_MI_CHK_NULL(gtSystemInfo);
+    auto skuTable = m_osInterface->pfnGetSkuTable(m_osInterface);
+    MHW_MI_CHK_NULL(skuTable);
+    if (MEDIA_IS_SKU(skuTable,FtrMediaIPSeparation))
+    {
+        MOS_OS_CHK_STATUS_RETURN(m_osInterface->pfnGetMediaEngineInfo(m_osInterface, mediaEngineInfo));
+    }
+    else
+    {
+        gtSystemInfo    = m_osInterface->pfnGetGtSystemInfo(m_osInterface);
+        MHW_MI_CHK_NULL(gtSystemInfo);
+    }
 
     if (forceVdbox != MOS_FORCE_VDBOX_NONE &&
         forceVdbox != MOS_FORCE_VDBOX_1 &&
@@ -535,13 +552,28 @@ MOS_STATUS MediaContext::CheckScalabilityOverrideValidity()
         MHW_VDBOX_IS_VDBOX_SPECIFIED(forceVdbox, MOS_FORCE_VDBOX_1, MOS_FORCEVDBOX_VDBOXID_BITSNUM, MOS_FORCEVDBOX_MASK, useVD1);
         MHW_VDBOX_IS_VDBOX_SPECIFIED(forceVdbox, MOS_FORCE_VDBOX_2, MOS_FORCEVDBOX_VDBOXID_BITSNUM, MOS_FORCEVDBOX_MASK, useVD2);
     }
-    if (!gtSystemInfo->VDBoxInfo.IsValid ||
-        (useVD1 && !gtSystemInfo->VDBoxInfo.Instances.Bits.VDBox0Enabled) ||
-        (useVD2 && !gtSystemInfo->VDBoxInfo.Instances.Bits.VDBox1Enabled))
+    if (MEDIA_IS_SKU(skuTable,FtrMediaIPSeparation))
     {
-        eStatus = MOS_STATUS_INVALID_PARAMETER;
-        MHW_ASSERTMESSAGE("the forced VDBOX is not enabled in current platform.");
-        return eStatus;
+        if (!mediaEngineInfo.VDBoxInfo.IsValid ||
+            (useVD1 && !mediaEngineInfo.VDBoxInfo.Instances.Bits.VDBox0Enabled) ||
+            (useVD2 && !mediaEngineInfo.VDBoxInfo.Instances.Bits.VDBox2Enabled))
+        {
+            eStatus = MOS_STATUS_INVALID_PARAMETER;
+            MHW_ASSERTMESSAGE("the forced VDBOX is not enabled in current platform.");
+            return eStatus;
+        }
+    }
+    else
+    {
+        if (!gtSystemInfo->VDBoxInfo.IsValid ||
+            (useVD1 && !gtSystemInfo->VDBoxInfo.Instances.Bits.VDBox0Enabled) ||
+            (useVD2 && !gtSystemInfo->VDBoxInfo.Instances.Bits.VDBox2Enabled))
+        {
+            eStatus = MOS_STATUS_INVALID_PARAMETER;
+            MHW_ASSERTMESSAGE("the forced VDBOX is not enabled in current platform.");
+            return eStatus;
+        }
+
     }
 
     return eStatus;
@@ -607,7 +639,7 @@ MOS_STATUS MediaContext::FindGpuNodeToUse(MediaFunction func, PMHW_VDBOX_GPUNODE
     }
 #if (_DEBUG || _RELEASE_INTERNAL)
     if (m_osInterface != nullptr && m_osInterface->bEnableDbgOvrdInVE &&
-        (!m_osInterface->bSupportVirtualEngine || !m_scalabilitySupported))
+        (!m_osInterface->bSupportVirtualEngine))
     {
         eStatus = MOS_STATUS_INVALID_PARAMETER;
         MHW_ASSERTMESSAGE("not support DebugOverrid on current OS or Platform.");
