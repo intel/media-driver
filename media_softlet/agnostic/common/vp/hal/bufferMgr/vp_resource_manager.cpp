intel/media-driver

commit f38f67cf657c477eea6cd2e5bea0725f9020ccf9

[VP] 30pRefdi case Linux
ADi case fail

media_softlet/agnostic/common/vp/hal/bufferMgr/vp_resource_manager.cpp
@@ -623,6 +623,7 @@ void VpResourceManager::InitSurfaceConfigMap()
     // outOfBound
     AddSurfaceConfig(true,  true,  false, true,  true,  false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_PAST_REF, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_FRAME0);
     AddSurfaceConfig(true,  true,  false, true,  true,  false, false, VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_NULL,     VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_NULL);
+
     // sfc disable
     AddSurfaceConfig(true,  false,  false, false, true,  false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_PAST_REF, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_OUTPUT);
     AddSurfaceConfig(true,  false,  true,  false, true,  false, false, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_NULL,     VEBOX_SURFACE_NULL,   VEBOX_SURFACE_NULL);
@@ -631,6 +632,18 @@ void VpResourceManager::InitSurfaceConfigMap()
     AddSurfaceConfig(true,  false,  false, false, true,  false, false, VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_PAST_REF, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_FRAME0);
     AddSurfaceConfig(true,  false,  true,  false, false, false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_NULL,     VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_NULL);
     AddSurfaceConfig(true,  false,  true,  false, false, false, false, VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_NULL,     VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_NULL);
+
+    //30i -> 30p sfc Enable
+    AddSurfaceConfig(false, true,   false, false, false, false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_NULL,     VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_NULL);
+    AddSurfaceConfig(false, true,   false, false, true,  false, false, VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_PAST_REF, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_FRAME0);
+    AddSurfaceConfig(false, true,   false, false, true,  false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_PAST_REF, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_FRAME0);
+    AddSurfaceConfig(false, true,   false, true,  true,  false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_PAST_REF, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_FRAME0);
+    AddSurfaceConfig(false, true,   false, true,  true,  false, false, VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_PAST_REF, VEBOX_SURFACE_FRAME1, VEBOX_SURFACE_FRAME0);
+    
+    //30i -> 30p sfc disable
+    AddSurfaceConfig(false, false,  false, false, true,  false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_NULL,     VEBOX_SURFACE_OUTPUT, VEBOX_SURFACE_NULL);
+    AddSurfaceConfig(false, false,  false, true,  true,  false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_NULL,     VEBOX_SURFACE_OUTPUT, VEBOX_SURFACE_NULL);
+    AddSurfaceConfig(false, false,  false, false, false, false, true,  VEBOX_SURFACE_INPUT,  VEBOX_SURFACE_NULL,     VEBOX_SURFACE_OUTPUT, VEBOX_SURFACE_NULL);
 }
 
 uint32_t VpResourceManager::GetHistogramSurfaceSize(VP_EXECUTE_CAPS& caps, uint32_t inputWidth, uint32_t inputHeight)
@@ -2098,6 +2111,15 @@ MOS_STATUS VpResourceManager::AssignVeboxResource(VP_EXECUTE_CAPS& caps, VP_SURF
         auto it = m_veboxSurfaceConfigMap.find(cfg.value);
         if (m_veboxSurfaceConfigMap.end() == it)
         {
+            VP_PUBLIC_ASSERTMESSAGE("SurfaceConfig is invalid, cfg.value = %d", cfg.value);
+            VP_PUBLIC_ASSERTMESSAGE("b64Di = %d, sfcEnable = %d, sameSample = %d, outOfBound = %d, pastframeAvailable = %d, future frameAvaliable = %d, FirstDiFiels = %d",
+                cfg.b64DI,
+                cfg.sfcEnable,
+                cfg.sameSample,
+                cfg.outOfBound,
+                cfg.pastFrameAvailable,
+                cfg.futureFrameAvailable,
+                cfg.firstDiField);
             VP_PUBLIC_CHK_STATUS_RETURN(MOS_STATUS_INVALID_PARAMETER);
         }
         auto surfaces = it->second;

media_softlet/agnostic/common/vp/hal/feature_manager/policy.cpp
@@ -608,12 +608,14 @@ MOS_STATUS Policy::GetCSCExecutionCapsDi(SwFilter* feature)
 
     SwFilterCsc* csc = dynamic_cast<SwFilterCsc*>(feature);
     VP_PUBLIC_CHK_NULL_RETURN(csc);
+    auto userFeatureControl = m_vpInterface.GetHwInterface()->m_userFeatureControl;
+    bool disableSfc         = userFeatureControl->IsSfcDisabled();
 
     VP_PUBLIC_CHK_STATUS_RETURN(GetCSCExecutionCaps(feature));
 
     VP_EngineEntry *cscEngine = &csc->GetFilterEngineCaps();
     VP_PUBLIC_CHK_NULL_RETURN(cscEngine);
-    if ((cscEngine->bEnabled && (cscEngine->SfcNeeded || cscEngine->VeboxNeeded)) ||
+    if (!disableSfc && (cscEngine->bEnabled && (cscEngine->SfcNeeded || cscEngine->VeboxNeeded)) ||
         !cscEngine->bEnabled && cscEngine->forceEnableForSfc)
     {
         cscEngine->bEnabled     = 1;
@@ -621,6 +623,16 @@ MOS_STATUS Policy::GetCSCExecutionCapsDi(SwFilter* feature)
         cscEngine->VeboxNeeded  = 0;
         cscEngine->RenderNeeded = 0;
     }
+    else
+    {
+        cscEngine->bEnabled     = 1;
+        cscEngine->SfcNeeded    = 0;
+        cscEngine->VeboxNeeded  = 0;
+        cscEngine->RenderNeeded = 1;
+        cscEngine->fcSupported  = 1;
+    }
+
+    PrintFeatureExecutionCaps(__FUNCTION__, *cscEngine);
     return MOS_STATUS_SUCCESS;
 }
 
@@ -823,7 +835,7 @@ MOS_STATUS Policy::GetCSCExecutionCaps(SwFilter* feature)
             cscEngine->SfcNeeded            = 0;
             cscEngine->VeboxNeeded          = 0;
             cscEngine->RenderNeeded         = 0;
-            if (sfcSupported)
+            if (sfcSupported && !disableSfc)
             {
                 cscEngine->forceEnableForSfc = 1;
             }
@@ -1118,7 +1130,7 @@ MOS_STATUS Policy::GetScalingExecutionCaps(SwFilter *feature, bool isHdrEnabled,
         {
             // for dst left and top non zero cases, should go SFC or Render Pipe
             scalingEngine->bEnabled             = 1;
-            scalingEngine->SfcNeeded            = isAlphaSettingSupportedBySfc;
+            scalingEngine->SfcNeeded            = !disableSfc && isAlphaSettingSupportedBySfc;
             scalingEngine->VeboxNeeded          = 0;
             scalingEngine->RenderNeeded         = 1;
             scalingEngine->fcSupported          = 1;
@@ -1136,7 +1148,7 @@ MOS_STATUS Policy::GetScalingExecutionCaps(SwFilter *feature, bool isHdrEnabled,
             scalingEngine->forceEnableForFc     = 1;
             scalingEngine->fcSupported          = 1;
             scalingEngine->hdrKernelSupported   = 1;
-            scalingEngine->sfcNotSupported      = !isAlphaSettingSupportedBySfc;
+            scalingEngine->sfcNotSupported      = disableSfc || !isAlphaSettingSupportedBySfc;
         }
 
         PrintFeatureExecutionCaps(__FUNCTION__, *scalingEngine);
@@ -1458,6 +1470,8 @@ MOS_STATUS Policy::GetDeinterlaceExecutionCaps(SwFilter* feature)
     VP_PUBLIC_CHK_NULL_RETURN(m_vpInterface.GetHwInterface());
     VP_PUBLIC_CHK_NULL_RETURN(m_vpInterface.GetHwInterface()->m_userFeatureControl);
     auto userFeatureControl = m_vpInterface.GetHwInterface()->m_userFeatureControl;
+    bool disableVeboxOutput = userFeatureControl->IsVeboxOutputDisabled();
+    bool disableSfc        = userFeatureControl->IsSfcDisabled();
 
     FeatureParamDeinterlace &diParams = swFilterDi->GetSwFilterParams();
     VP_EngineEntry &diEngine = swFilterDi->GetFilterEngineCaps();
@@ -1481,19 +1495,21 @@ MOS_STATUS Policy::GetDeinterlaceExecutionCaps(SwFilter* feature)
         diEngine.bEnabled     = 1;
         diEngine.RenderNeeded = 1;
         diEngine.fcSupported  = 1;
+        diEngine.VeboxNeeded  = 0;
         PrintFeatureExecutionCaps(__FUNCTION__, diEngine);
         return MOS_STATUS_SUCCESS;
     }
 
-    if (diParams.diParams                                                       &&
-        !MOS_IS_ALIGNED(MOS_MIN((uint32_t)diParams.heightInput, (uint32_t)diParams.rcSrc.bottom), 4) &&
+    if (nullptr == diParams.diParams                                            ||                                                    
+        (!MOS_IS_ALIGNED(MOS_MIN((uint32_t)diParams.heightInput, (uint32_t)diParams.rcSrc.bottom), 4) &&
         (diParams.formatInput == Format_P010                                    ||
          diParams.formatInput == Format_P016                                    ||
-         diParams.formatInput == Format_NV12))
+         diParams.formatInput == Format_NV12)))
     {
         diEngine.bEnabled     = 0;
         diEngine.RenderNeeded = 0;
         diEngine.fcSupported  = 0;
+        diEngine.VeboxNeeded  = 0;
         PrintFeatureExecutionCaps(__FUNCTION__, diEngine);
         return MOS_STATUS_SUCCESS;
     }
@@ -1518,13 +1534,34 @@ MOS_STATUS Policy::GetDeinterlaceExecutionCaps(SwFilter* feature)
     }
     else
     {
-        diEngine.bEnabled     = 1;
-        diEngine.RenderNeeded = 1;
-        diEngine.fcSupported  = 1;
-        diEngine.VeboxNeeded  = 1;
+        if (diParams.diParams->DIMode == DI_MODE_BOB)
+        {
+            diEngine.bEnabled     = 1;
+            diEngine.RenderNeeded = 1;
+            diEngine.fcSupported  = 1;
+
+            // to align with case design, if disableSfc == 1, force di to render
+            if (disableSfc)
+            {
+                diEngine.VeboxNeeded = 0;
+                VP_PUBLIC_NORMALMESSAGE("Force DI to render.");
+            }
+            else
+            {
+                diEngine.VeboxNeeded = 1;
+            }
+        }
+        else
+        {
+            diEngine.bEnabled     = 1;
+            diEngine.RenderNeeded = 0;
+            diEngine.fcSupported  = 0;
+            diEngine.VeboxNeeded  = 1;
+        }
     }
 
     PrintFeatureExecutionCaps(__FUNCTION__, diEngine);
+    VP_PUBLIC_NORMALMESSAGE("Di mode = %d", diParams.diParams->DIMode);
     return MOS_STATUS_SUCCESS;
 }
 
@@ -1795,7 +1832,7 @@ MOS_STATUS Policy::GetColorFillExecutionCaps(SwFilter* feature)
     engine.fcSupported = 1;
     // For disableSfc case, sfc will be filtered in SwFilterColorFill::GetCombinedFilterEngineCaps
     // with scaling setting.
-    engine.SfcNeeded = 1;    // For SFC, the parameter in scaling is used.
+    engine.SfcNeeded = !disableSfc;    // For SFC, the parameter in scaling is used.
 
     PrintFeatureExecutionCaps(__FUNCTION__, engine);
     return MOS_STATUS_SUCCESS;
@@ -2017,7 +2054,7 @@ MOS_STATUS Policy::InitExecuteCaps(VP_EXECUTE_CAPS &caps, VP_EngineEntry &engine
         }
         else
         {
-            caps.bSFC = engineCaps.nonVeboxFeatureExists;
+            caps.bSFC = !engineCapsOutputPipe.sfcNotSupported && engineCaps.nonVeboxFeatureExists;
         }
     }
     else

commit 56f772124b7d6936cd6fc7451d59e52477f6195f

[VP] Fix VEBOX surface config crash in 60i->30p deinterlacing
When performing 60i->30p deinterlacing (non-bob, b60fpsDi=false)
with resize on pure softlet platforms (MTL+), m_sameSamples could
be true, creating a VEBOX_SURFACES_CONFIG not registered in
InitSurfaceConfigMap(). This caused a segfault in
AssignVeboxResource() when looking up the config.

Gate m_sameSamples by b60fpsDi: only 60fps DI (60i->60p) paths
use the sameSample flag, since all 60i->30p entries in the
surface config map have sameSample=false.

Bug was introduced in f38f67cf6 ("[VP] 30pRefdi case Linux",
2022-11-09) which added 60i->30p config entries without
sameSample=true variants. The issue affects all pure softlet
platforms (MTL, ARL, LNL, BMG, PTL, NVL) and is codec-agnostic.

media_softlet/agnostic/common/vp/hal/bufferMgr/vp_resource_manager.cpp
@@ -2561,7 +2561,11 @@ MOS_STATUS VpResourceManager::AssignVeboxResource(VP_EXECUTE_CAPS& caps, VP_SURF
     if (caps.bDI || caps.bDiProcess2ndField)
     {
         bool b60fpsDi = resHint.b60fpsDi || caps.bDiProcess2ndField;
-        VEBOX_SURFACES_CONFIG cfg(b60fpsDi, caps.bSFC, m_sameSamples, m_outOfBound, m_currentFrameIds.pastFrameAvailable,
+        // sameSample path is only meaningful for 60fps DI workflows.
+        // For 30i->30p (single-field) DI, forcing sameSample can select an invalid
+        // VEBOX surface config combination.
+        const bool sameSamplesForConfig = b60fpsDi ? m_sameSamples : false;
+        VEBOX_SURFACES_CONFIG cfg(b60fpsDi, caps.bSFC, sameSamplesForConfig, m_outOfBound, m_currentFrameIds.pastFrameAvailable,
             m_currentFrameIds.futureFrameAvailable, IsInterleaveFirstField(inputSurface->SampleType));
         auto it = m_veboxSurfaceConfigMap.find(cfg.value);
         if (m_veboxSurfaceConfigMap.end() == it)